f01199438ee1ecd3f213410dd4b74e3013eaeecf
Aran Dunkley <aran@organicdesign.co.nz>
2009-08-01 01:16:31 +0000

    add loop for double pass
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@46 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- dcs-parser.pl
+++ dcs-parser.pl
@@ -11,7 +11,7 @@ wikiLogin( $wiki, $user, $pass ) or die "Couldn't log into wiki!";
 
 sub doPage {
 	if ( $text ) {
-		if ( $title ) {
+		if ( $pass and $title ) {
 			wikiEdit( $wiki, $title, $text, "Content imported from $file" );
 			sleep 1 if $changed; # delay to give time for the bot to update terms links
 		}
@@ -22,30 +22,34 @@ sub doPage {
 	}
 }
 
-$line = 1;
-for $file ( glob "$dir/*" ) {
+for $pass ( 0..1 ) {
 
-	print "Processing \"$file\"\n";
-	logAdd( "Processing \"$file\"" );
-	open INPUT, '<', $file or die "Could not open input file '$file'!";
+	$line = 1;
+	for $file ( glob "$dir/*" ) {
 
-	$title = '';
-	$text  = '';
-	for ( <INPUT> ) {
+		$comment = "PASS-" . ( $pass+1 ) . ": Processing \"$file\"";
+		print "$comment\n";
+		logAdd( $comment );
+		open INPUT, '<', $file or die "Could not open input file '$file'!";
 
-		if ( /^>>>\s*(.+)\s*<<<\s*/ ) {
-			&doPage;
-			$title = $1;
+		$title = '';
+		$text  = '';
+		for ( <INPUT> ) {
+
+			if ( /^>>>\s*(.+)\s*<<<\s*/ ) {
+				&doPage;
+				$title = $1;
+			}
+			elsif ( /^>>>/ or /<<<\s*$/ ) {
+				logAdd( "Error: badly formed title markup on line $line" );
+			}
+			else {
+				$text .= $_;
+			}
+			
+		$line++;
 		}
-		elsif ( /^>>>/ or /<<<\s*$/ ) {
-			logAdd( "Error: badly formed title markup on line $line" );
-		}
-		else {
-			$text .= $_;
-		}
-		
-	$line++;
+		close INPUT;
+		&doPage;
 	}
-	close INPUT;
-	&doPage;
 }
