0d518e1e09ce99cb3f5bd4f5516745130663e072
Aran Dunkley <aran@organicdesign.co.nz>
2009-08-01 03:07:02 +0000

    comments and caretaker loop structure
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@58 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- dcs-events.pl
+++ dcs-events.pl
@@ -31,11 +31,51 @@ sub onRevisionInsertComplete {
 # Parse some article text and update terms links
 sub parseContent {
 	my $text = shift;
+	
+	# read MediaWiki:TermsList
+	
+	# sort
+	
+	# loop thru them
+	
+		# if ( target ) 
+	
+		# check for each term in unlinked form for ones needing terms
+		
+		# check for linked ones to "undo" if marked as not linking (no target)
+	
 	return $text;
 }
 
 # Scan all articles in all wikis and update links in each
 sub scanArticles {
+
+	# Build a list of all articles in the wiki
+	my $sth = $::db->prepare( 'SELECT page_id FROM ' . $::dbpre . 'page WHERE page_title = "Zhconversiontable"' );
+	$sth->execute();
+	my @row = $sth->fetchrow_array;
+	$sth->finish;
+	my $first = $row[0]+1;
+	my $sth = $dbh->prepare( 'SELECT page_id FROM ' . $::dbpre . 'page ORDER BY page_id DESC' );
+	$sth->execute();
+	@row = $sth->fetchrow_array;
+	$sth->finish;
+	my $last = $row[0];
+	@list = ( $first .. $last );
+
+	# Loop through all articles
+	my $sthid = $dbh->prepare( 'SELECT page_namespace,page_title,page_is_redirect FROM ' . $dbpre . 'page WHERE page_id=?' );
+	my $done = 'none';
+	for ( @list ) {
+		$sthid->execute( $_ );
+		@row = $sthid->fetchrow_array;
+		my @comments = ();
+		my $title = $ns{$row[0]} ? $ns{$row[0]}.':'.$row[1] : $row[1];
+		if ( $title && ( $row[2] == 0 ) ) {
+			print "$title\n";
+			parseContent( $text );
+		}
+	}
 }
 
 1;
