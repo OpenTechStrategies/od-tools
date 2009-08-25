815981d34d7d1d0b1bbe744a0270f9f50fa65bae
Aran Dunkley <aran@organicdesign.co.nz>
2009-08-25 04:35:34 +0000

    Update fs stats in Template:FileSystemUsage article
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@67 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- unison.pl
+++ unison.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl
 use Expect;
 require( '/var/www/tools/wikid.conf' );
+require( '/var/www/tools/wiki.pl' );
 $bak_user = $name unless defined $bak_user;
+$wikiuser = $name unless defined $wikiuser;
+wikiLogin( $wiki, $wikiuser, $wikipass ) if defined $wikipass;
 
+$out = '';
 for $dir ( @bak_paths ) {
+	$out .= qx( du -sh $dir ) . "\n";
 	$cmd = "unison $dir ssh://$bak_user\@$bak_server$dir -batch -force $dir -log -logfile /var/log/syslog";
 	$exp = Expect->spawn( $cmd );
 	$exp->expect(
@@ -14,5 +19,4 @@ for $dir ( @bak_paths ) {
 	$exp->soft_close();
 }
 
-
-
+wikiEdit( $wiki, 'Template:FileSystemUsage', 'Update usage statistics' );
