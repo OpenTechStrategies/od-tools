29329a5ebcdcbaffbe26fabe48ffe488cd198865
Aran Dunkley <aran@organicdesign.co.nz>
2009-08-25 04:45:21 +0000

    use separate array for stat paths
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@68 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- unison.pl
+++ unison.pl
@@ -4,9 +4,7 @@ require( '/var/www/tools/wikid.conf' );
 require( '/var/www/tools/wiki.pl' );
 $bak_user = $name unless defined $bak_user;
 $wikiuser = $name unless defined $wikiuser;
-wikiLogin( $wiki, $wikiuser, $wikipass ) if defined $wikipass;
 
-$out = '';
 for $dir ( @bak_paths ) {
 	$out .= qx( du -sh $dir ) . "\n";
 	$cmd = "unison $dir ssh://$bak_user\@$bak_server$dir -batch -force $dir -log -logfile /var/log/syslog";
@@ -19,4 +17,10 @@ for $dir ( @bak_paths ) {
 	$exp->soft_close();
 }
 
-wikiEdit( $wiki, 'Template:FileSystemUsage', 'Update usage statistics' );
+if ( defined $wikipass ) {
+	wikiLogin( $wiki, $wikiuser, $wikipass );
+	my $text = '';
+	$text .= qx( du -sh $_ ) . "\n" for @stat_paths;
+	$text = "<pre>\n$text</pre>";
+	wikiEdit( $wiki, 'Template:FileSystemUsage', $text, 'Update usage statistics' );
+}
