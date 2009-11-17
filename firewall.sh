daf1277c26dea8373c95b51a059205e00e8c4628
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-17 05:25:09 +0000

    syntax changed for negations
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@119 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- firewall.sh
+++ firewall.sh
@@ -11,7 +11,7 @@ iptables -A INPUT -i lo -j ACCEPT
 
 # Allow established connections, and those not coming from the outside
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -m state --state NEW -i ! eth0 -j ACCEPT
+iptables -A INPUT -m state --state NEW ! -i eth0 -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Allow outgoing connections from the LAN side.
