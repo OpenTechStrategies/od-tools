463a07c0c787a904980115a99e25367b9915bd4a
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-17 04:12:22 +0000

    put eth0 and 1 into EXT and INT variables at start
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@117 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+EXT = "eth0"
+INT = "eth1"
  
 # delete all existing rules.
 iptables -F
@@ -11,20 +14,20 @@ iptables -A INPUT -i lo -j ACCEPT
  
 # Allow established connections, and those not coming from the outside
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -m state --state NEW -i ! eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state NEW -i ! $EXT -j ACCEPT
+iptables -A FORWARD -i $EXT -o $INT -m state --state ESTABLISHED,RELATED -j ACCEPT
  
 # Allow outgoing connections from the LAN side.
-iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+iptables -A FORWARD -i $INT -o $EXT -j ACCEPT
  
 # Masquerade.
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $EXT -j MASQUERADE
  
 # Dont forward from the outside to the inside.
-iptables -A FORWARD -i eth0 -o eth0 -j REJECT
+iptables -A FORWARD -i $INT -o $EXT -j REJECT
  
 # Example pinhole: forward packets on port 5900 to 192.168.1.100
-iptables -A FORWARD -i eth0 -o eth1 -p tcp --dport 5900 -j ACCEPT
+iptables -A FORWARD -i $EXT -o $INT -p tcp --dport 5900 -j ACCEPT
 iptables -A PREROUTING -t nat -p tcp --dport 5900 -j DNAT --to-destination 192.168.1.100
  
 # Enable routing.
