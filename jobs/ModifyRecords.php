dca40521a2cb834679dc264bcac4c5a478eef43e
Aran Dunkley <aran@organicdesign.co.nz>
2009-12-14 02:23:14 +0000

    finish ModifyRecords form
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@146 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- jobs/ModifyRecords.php
+++ jobs/ModifyRecords.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * Adds MediaWiki hooks handling ModifyRecords form
+ * Adds MediaWiki hooks for handling custom form for the ModifyRecords job type
  */
 if ( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
 
-$wgHooks['WikidAdminTypeFormRender_ModifyRecords'][] = 'wfModifyRecords_Render';
-$wgHooks['WikidAdminTypeFormProcess_ModifyRecords'][] = 'wfModifyRecords_Process';
 
 /**
- * Render a form in Special:WikidAdmin for the AAPImport type
+ * Render the form
  */
+$wgHooks['WikidAdminTypeFormRender_ModifyRecords'][] = 'wfModifyRecords_Render';
 function wfModifyRecords_Render( &$html ) {
 	$html = '<table>';
 	$html .= '<tr><td>Action:</td><td><select name="wpChangeType">';
-	$html .= '<option value="name">Change a record name and all values referencing it</option>';
-	$html .= '<option value="name">Change all occurences of a particular value</option>';
-	$html .= '<option value="name">Change a field name and all references to it</option>';
-	$html .= '<option value="name">Change the name of a record type and all references to it</option>';
+	$html .= '<option value="record">Change a record name and all values referencing it</option>';
+	$html .= '<option value="value">Change all occurences of a particular value</option>';
+	$html .= '<option value="field">Change a field name and all references to it</option>';
+	$html .= '<option value="type">Change the name of a record type and all references to it</option>';
 	$html .= '</select></td></tr>';
 	$html .= '<tr><td>From:</td><td><input name="wpFrom" /></td></tr>';
 	$html .= '<tr><td>To:</td><td><input name="wpTo" /></td></tr>';
@@ -25,17 +24,13 @@ function wfModifyRecords_Render( &$html ) {
 }
 
 /**
- * Process posted data from ImportCSV form
+ * Process submitted form
  */
+$wgHooks['WikidAdminTypeFormProcess_ModifyRecords'][] = 'wfModifyRecords_Process';
 function wfModifyRecords_Process( &$job, &$start ) {
 	global $wgRequest, $wgSiteNotice;
-	$job['template'] = $wgRequest->getText( 'template' );
-	$job['title'] = $wgRequest->getText( 'format' );
-
-	# Start the job if a valid file was specified, error if not
-	if ( !$start = is_file( $job['file'] ) ) $wgSiteNotice = "<div class='errorbox'>No valid file specified, job not started!</div>";
-
-	if ( $wgSiteNotice ) $wgSiteNotice .= "<div style='clear:both'></div>";
-
-	return true;
+	$job['ChangeType'] = $wgRequest->getText( 'wpChangeType' );
+	$job['From'] = $wgRequest->getText( 'wpFrom' );
+	$job['To'] = $wgRequest->getText( 'wpTo' );
+	return $start = true;
 }
