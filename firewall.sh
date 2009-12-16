d3223b3cb2e6e79c775a5afe7f31b259afbb3437
Aran Dunkley <aran@organicdesign.co.nz>
2009-12-16 23:08:02 +0000

    update firewall to newly tested working state
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@159 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- firewall.sh
+++ firewall.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
-# delete all existing rules.
+# Specify which network interfaces are internal and external
+set INT = "eth0"
+set EXT = "eth1"
+
+# Delete all existing rules
 iptables -F
 iptables -t nat -F
 iptables -t mangle -F
 iptables -X
-
-# Always accept loopback traffic
-iptables -A INPUT -i lo -j ACCEPT
+iptables -t nat -X
 
 # Allow established connections, and those not coming from the outside
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -m state --state NEW ! -i eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state NEW ! -i "$EXT" -j ACCEPT
+iptables -A FORWARD -i "$EXT" -o "$INT" -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Allow outgoing connections from the LAN side.
-iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+# Allow outgoing connections from the LAN side
+iptables -A FORWARD -i "$INT" -o "$EXT" -j ACCEPT
 
-# Masquerade.
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Masquerade
+iptables -t nat -A POSTROUTING -o "$EXT" -j MASQUERADE
 
-# Dont forward from the outside to the inside.
-iptables -A FORWARD -i eth0 -o eth0 -j REJECT
+# Don't forward packets from the outside to the inside
+iptables -A FORWARD -i "$EXT" -o "$INT" -j REJECT
 
 # Example pinhole: forward packets on port 5900 to 192.168.1.100
-iptables -A FORWARD -i eth0 -o eth1 -p tcp --dport 5900 -j ACCEPT
-iptables -A PREROUTING -t nat -p tcp --dport 5900 -j DNAT --to-destination 192.168.1.100
+#iptables -A FORWARD -i "$EXT" -o "$INT" -p tcp --dport 5900 -j ACCEPT
+#iptables -A PREROUTING -t nat -p tcp --dport 5900 -j DNAT --to-destination 192.168.1.100
 
-# Enable routing.
+# Enable ip-forwarding in the kernel
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
