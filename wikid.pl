cfa7a280dcc0cfe4e13c8396872d6b92dd48d5dd
Aran Dunkley <aran@organicdesign.co.nz>
2010-01-03 00:40:30 +0000

    dovecot ignore regex must match whole name
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@260 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- wikid.pl
+++ wikid.pl
@@ -33,7 +33,7 @@ $::daemon   = 'wikid';
 $::host     = uc( hostname );
 $::name     = hostname;
 $::port     = 1729;
-$::ver      = '3.11.1'; # 2009-01-03
+$::ver      = '3.11.2'; # 2009-01-03
 $::log      = "$::dir/$::daemon.log";
 $::wkfile   = "$::dir/$::daemon.work";
 $::motd     = "Hail Earthlings! $::daemon-$::ver is in the heeeeeouse! (rock)" unless defined $::motd;
@@ -550,7 +550,7 @@ sub onFileChanged {
 	my $newsize = shift;
 	my $text    = '';
 	my $msg     = '';
-	my @userfilter = ( 'root', 'nobody', 'fit', 'Bender', 'dcs', 'kg-lan', 'od-lan' );
+	my @userfilter = ( $::netuser, 'root', 'nobody', 'fit' );
 
 	# Read in difference
 	if ( $newsize > $oldsize and open FH, '<', $file ) {
