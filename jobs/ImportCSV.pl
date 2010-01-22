23092c044ab311f319eb56932e74ae06c343714f
Aran Dunkley <aran@organicdesign.co.nz>
2010-01-22 05:43:46 +0000

    handle multi-line values
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@277 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- jobs/ImportCSV.pl
+++ jobs/ImportCSV.pl
@@ -7,53 +7,67 @@
 
 # Set the file pointer to start of the file (i.e. first line)
 sub initImportCSV {
-	my $file = $$::job{'file'};
-	my $lines = qx( wc -l "$file" );
-	$$::job{'fptr'} = 0;
-	$$::job{'length'} = int( $lines );
+	$$::job{fptr} = 0;
+	$$::job{rows} = 0;
 	1;
 }
 
 # Import the current line from the input CSV file
 sub mainImportCSV {
-	my $wiki = $$::job{'wiki'};
-	my $file = $$::job{'file'};
-	my $fptr = $$::job{'fptr'};
-	my $wptr = $$::job{'wptr'};
-	my $tmpl = $$::job{'template'};
+	my $wiki = $$::job{wiki};
+	my $file = $$::job{file};
+	my $fptr = $$::job{fptr};
+	my $rows = $$::job{rows}++;
+	my $tmpl = $$::job{template};
 	my @data = ();
 
 	# Read the CSV record from the indexed offset into an array
 	if ( open INPUT, "<$file" ) {
+
+		# Read a line accountung for multiline items in quotes
 		seek INPUT, $fptr, 0;
-		@data = split /\t/, <INPUT>;
-		$$::job{'fptr'} = tell INPUT;
-		s/^\s*(.*?)\s*$/$1/s for @data;
+		my $line = '';
+		my $chr = '';
+		my $i = 0;
+		my $q = 0;
+		do {
+			$i = read INPUT, $chr, 1;
+			$q++ if $chr eq '"';
+			$line .= $chr;
+		} while( $i && ( $q%2 || ( $chr ne "\r" && $chr ne "\n" ) ) );
+		$$::job{fptr} = tell INPUT;
 		close INPUT;
+		workStopJob() if $i == 0;
+
+		# Split and trim the line and remove quotes if any
+		@data = split /\t/, $line;
+		s/^\s*"?(.*?)"?\s*$/$1/s for @data;
+		logAdd($line);
+
 	} else {
 		workLogError( "Couldn't read input file \"$file\", job aborted!" );
 		workStopJob();
 	}
 
 	# If this is the first row, define the columns and a reverse lookup
-	if ( $wptr == 0 ) {
-		$$::job{'cols'} = \@data;
+	if ( $fptr == 0 ) {
+		$$::job{cols} = \@data;
 		my %lut = ();
 		$lut{lc $data[$_]} = $_ for 0 .. $#data;
-		$$::job{'lut'} = \%lut;
+		$$::job{lut} = \%lut;
 	}
 
 	# Otherwise construct record as wikitext and insert into wiki
 	else {
 
 		# Determine title for the record
-		my $title = $$::job{'title'};
+		my $title = $$::job{title};
 		if ( $title ) {
 			$title =~ s/\$(\w+)/ jobImportCSVBuildTitle( $1, \@data ) /eg;
 		} else { $title = wikiGuid() }
 
 		# Construct the template syntax
-		my @cols = @{ $$::job{'cols'} };
+		my @cols = @{ $$::job{cols} };
 		my $text = "\{\{$tmpl\n";
 		$text .= " | $cols[$_] = $data[$_]\n" for 0 .. $#cols;
 		$text .= "\}\}";
@@ -67,10 +81,10 @@ sub mainImportCSV {
 			$minor = 0;
 			$comment = "Article content replaced from \"$leaf\" import";
 		}
-		$$::job{'revisions'}++ if wikiEdit( $wiki, $title, $text, $comment, $minor ) and $cur ne $text;
+		$$::job{revisions}++ if wikiEdit( $wiki, $title, $text, $comment, $minor ) and $cur ne $text;
 	}
 
-	$$::job{'status'} = "Record $ptr imported";
+	$$::job{status} = "Record $rows imported";
 	1;
 }
 
@@ -80,7 +94,7 @@ sub mainImportCSV {
 sub jobImportCSVBuildTitle {
 	my $i = shift;
 	my $data = shift;
-	my %lut = %{$$::job{'lut'}};
+	my %lut = %{$$::job{lut}};
 	my $title = $$data[ $i =~ /\D/ ? $lut{lc $i} : $i - 1 ];
 	$title =~ s/ +/ /g;
 	return $title;
