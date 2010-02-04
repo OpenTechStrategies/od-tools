f24dde894fd44a9fd1ca30f70ef8cc62a13cf013
Aran Dunkley <aran@organicdesign.co.nz>
2010-02-04 07:58:49 +0000

    the main regex's
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@293 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- private2ewgod.pl
+++ private2ewgod.pl
@@ -8,39 +8,61 @@ require "wikid.conf";
 wikiLogin( $ewgsrc, $ewguser, $ewgpass );
 wikiLogin( $ewgdst, $ewguser, $ewgpass );
 
+my $comment = "Imported by private2ewgod.pl";
+
 # Projects
 my @titles = wikiParse( $ewgsrc, '{{#dpl:category=KIWIGREEN Limited|category=Projects}}', 1 );
 for my $title ( @titles ) {
-	
-	# Change Organisation from KG to EWG
-	
-	# Change Leader and Members to full names
-	
+	my $text = wikiRawPage( $ewgsrc, $title );
+
+	&changeOrganisation;
+	&changePeople;
+
+	wikiEdit( $ewgdst, $title, $text, $comment );
 }
 
-# Tasks
+# Issues
 my @titles = wikiParse( $ewgsrc, '{{#dpl:category=KIWIGREEN Limited|category=Issues}}', 1 );
 for my $title ( @titles ) {
-	
-	# Change Organisation from KG to EWG
-	
-	# Change AssignedTo and Attention to full names
-	
+	my $text = wikiRawPage( $ewgsrc, $title );
+
+	&changeOrganisation;
+	&changePeople;
+	&changeIssue;
+
+	wikiEdit( $ewgdst, $title, $text, $comment );
 }
 
 # Activities
 my @titles = wikiParse( $ewgsrc, '{{#dpl:category=KIWIGREEN Limited|category=Activities}}', 1 );
 for my $title ( @titles ) {
-	
-	# Change Organisation from KG to EWG
-	
-	# Change Participants to full names
-	
-	# Change Issue parameter to Task
-	
+	my $text = wikiRawPage( $ewgsrc, $title );
+
+	&changeOrganisation;
+	&changePeople;
+	&changeIssue;
+
+	wikiEdit( $ewgdst, $title, $text, $comment );
 }
 
 
 # Change first names to full names
-sub fullNames {
+sub changePeople {
+	$text =~ s|Aran(?!= Dunkley)|Aran DUnkley|g;
+	$text =~ s|Jack(?!= Henderson)|Jack Henderson|g;
+	$text =~ s|Milan(?!= Holzapfel)|Milan Holzapfel|g;
+	$text =~ s|Rob(?!=ert)|Robert Carter|g;
+	$text =~ s|Dana(?!= Darwin)|Dana Darwin|g;
+	$text =~ s|Sven|Marcus Davy|g;
+}
+
+# Change organisation
+sub changeOrganisation {
+	$text =~ s|kiwigreen limited|Earthwise Group Ltd|ig;
+}
+
+# Change issues
+sub changeIssue {
+	$text =~ s|(?<=\{\{)Issue(?=\W)|Task|;
+	$text =~ s|^(\s*\|\s*)Issue(\s*=)|$1Task $2|mg;
 }
