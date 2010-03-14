83d41dfd1000f8a0bee8dfb72c63c46f06a48c7e
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-14 23:57:05 +0000

    wrong path in tar
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@317 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- update-tools.sh
+++ update-tools.sh
@@ -4,5 +4,5 @@ rm od-tools.tgz
 wget http://www.organicdesign.co.nz/files/od-tools.tgz
 tar -zxf od-tools.tgz
 cd /var/www/tools        
-cp -ru /tmp/var/www/od-tools/* ./
+cp -ru /tmp/var/www/tools/* ./
 
