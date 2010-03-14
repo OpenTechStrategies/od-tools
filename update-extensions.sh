5d8b7d5f8d1434ce27486e3746dd5ae8056ee26b
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-14 23:33:08 +0000

    debug
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@315 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- update-extensions.sh
+++ update-extensions.sh
@@ -3,7 +3,8 @@ cd /tmp
 rm od-extensions.tgz
 wget http://www.organicdesign.co.nz/files/od-extensions.tgz
 tar -zxf od-extensions.tgz
-cp -ru var/www/od-extensions /var/www/extensions
+mkdir /var/www/extensions
 cd /var/www/extensions
+cp -ru /tmp/var/www/od-extensions/* ./
 chown -R www-data:www-data ./
 chmod -R 755 ./
