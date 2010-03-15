ef8d49942f96039cfaa501399e83520b73e923ce
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-15 07:21:08 +0000

    allowwd to supply gzipped
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@319 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- update-content.sh
+++ update-content.sh
@@ -2,5 +2,4 @@
 cd /tmp
 rm od-content.xml.gz
 wget http://www.organicdesign.co.nz/files/od-content.xml.gz
-gunzip od-content.xml.gz
-php /var/www/domains/localhost/wiki/maintenance/importDump.php /tmp/od-content.xml
+php /var/www/domains/localhost/wiki/maintenance/importDump.php /tmp/od-content.xml.gz
