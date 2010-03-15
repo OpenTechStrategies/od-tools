993320a1048f1220d325c4d100a247080e17f3c7
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-15 07:14:45 +0000

    start content importer
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@318 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

---
+++ update-content.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /tmp
+rm od-content.xml.gz
+wget http://www.organicdesign.co.nz/files/od-content.xml.gz
+gunzip od-content.xml.gz
+php /var/www/domains/localhost/wiki/maintenance/importDump.php /tmp/od-content.xml
