a1052874129234444dc3af9effbaf975da44e2f7
Aran Dunkley <aran@organicdesign.co.nz>
2010-05-05 00:43:33 +0000

    add a script to backup config, fs and DB's immediately
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@343 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

---
+++ backup-now.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use POSIX qw(strftime setsid);
+require "/var/www/tools/wikid.conf";
+
+$dir  = '/backup';
+$date = strftime( '%Y-%m-%d', localtime );
+
+# Return size of passed file in MB
+sub size { return (int([stat shift]->[7]/104857.6+0.5)/10).'MB'; }
+
+# Backup and compress databases
+$s7z = "all-$date.sql.7z";
+$sql = "$dir/all.sql";
+qx( mysqldump -u $wgDBuser --password='$wgDBpassword' -A >$sql );
+qx( 7za a $dir/$s7z $sql );
+qx( chmod 644 $dir/$s7z );
+print "\n\nDB backup: $s7z (".size($sql)."/".size("$dir/$s7z").")\n";
+
+
+# Backup config files
+$conf = join( ' ',
+	"/var/www/tools/wikid.conf",
+	"/var/www/tools/backup.pl",
+		"/etc/apache2/sites-available",
+	"/etc/exim4",
+	"/etc/bind9",
+	"/var/cache/bind",
+	"/etc/ssh/sshd_config",
+	"/etc/samba/smb.conf",
+	"/etc/crontab",
+	"/etc/network/interfaces"
+);
+$tgz = "config-$date.tgz";
+qx( tar -czf $dir/$tgz $conf );
+print "\n\nConfig backup: $tgz (".size("$dir/$tgz").")\n";
+
+# Tmp file to use for tar's before compressed
+$tar = "$dir/tmp.tar";
+
+# Backup and compress wiki/web structure
+$t7z = "www-$date.t7z";
+qx( tar -cf $tar /var/www -X /var/www/tools/backup-exclusions );
+qx( 7za a $dir/$t7z $tar );
+qx( chmod 644 $dir/$t7z );
+comment "FS backup: $t7z (".size($tar)."/".size("$dir/$t7z").")";
