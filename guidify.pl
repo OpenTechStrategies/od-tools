33a28168f653f5ce38df6e2b08e6f7c45bc055ca
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-03 06:59:03 +0000

    main strcuture of guidify.pl
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@371 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- guidify.pl
+++ guidify.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 #
+# guidify.pl - converts a Wiki Organisation to all-GUID record names
+#
 # Copyright (C) 2010 Aran Dunkley and others.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,3 +18,60 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 # http://www.gnu.org/copyleft/gpl.html
+#
+require( '/var/www/tools/wiki.pl' );
+require( '/var/www/tools/wikid.conf' );
+
+# Login to the wiki
+wikiLogin( $wiki, $wikiuser, $wikipass );
+
+
+# Get record types (Category:Records)
+
+# Read all instances into a hash by title
+
+# Loop through the instances
+while ( ( $title, $record ) = each %records ) {
+	
+	# Check if the type is GUID
+	if ( $title =~ /^\d{8}-\W{5}$/ ) {
+
+		# Create a GUID with date part matching the creation date of the record
+		my @first = wikiFirstEdit( $wiki, $title );
+		my $guid = wikiGuid( $first[0] );
+		$$record{guid} = $guid;
+		
+		# Scan all instances for references to the old title and update their content
+		while ( ( $t2, $r2 ) = each %records ) {
+			my $tmp = $$r2{content};
+			$$r2{content} =~ s/$title/$guid/g;
+			$$r2{changed} = 1 if $$r2{content} ne $tmp;
+		}
+		
+	}
+	
+}
+
+# Loop through all instances again and write changes to the wiki
+while ( ( $title, $record ) = each %records ) {
+	
+	# If the record has a new GUID, rename it, add a name parameter and free the old title
+	if ( $$record{guid} ) {
+
+		# Move the instance to a GUID name (& its talk)
+		
+		# Add a name parameter set to the old title
+		
+		# Delete the redirect (and talk redirect)
+	}
+	
+	# If it's not a new GUID, but does have changes, update it
+	elsif ( $$record{changed} ) {
+		wikiEdit( $wiki, $title, $$record{content}, "References changed to GUIDs by [[guidify.pl]]" );
+	}
+
+}
+
+
+# Log out
+wikiLogout( $wiki );
