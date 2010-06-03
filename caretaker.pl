c1b6ade75ab746f221e538d4ff8b96f7700a824f
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-03 03:43:08 +0000

    update licence text
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@368 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- caretaker.pl
+++ caretaker.pl
@@ -1,9 +1,24 @@
 #!/usr/bin/perl
-# caretaker.pl - Organic Design wiki caretaker script{{perl}}
 #
-# - See http://www.organicdesign.co.nz/caretaker.pl
-# - copyright © 2007 Aran Dunkley
-# - license GNU General Public Licence 2.0 or later
+# caretaker.pl - Organic Design wiki caretaker script
+#
+# Copyright (C) 2007-2010 Aran Dunkley and others.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# http://www.gnu.org/copyleft/gpl.html
+#
 use DBI;
 require "wiki.pl";
 require "wikid.conf";
--- wiki.pl
+++ wiki.pl
@@ -1,10 +1,26 @@
 #!/usr/bin/perl
-# - Licenced under LGPL (http://www.gnu.org/copyleft/lesser.html)
-# - Authors: [http://www.organicdesign.co.nz/Nad Nad], [http://www.organicdesign.co.nz/Sven Sven]
+#
+# Copyright (C) 2008-2010 Aran Dunkley, Marcus Davy and others.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# http://www.gnu.org/copyleft/gpl.html
+#
 # - Source:  http://www.organicdesign.co.nz/wiki.pl
 # - Started: 2008-03-16
 # - Tested versions: 1.6.10, 1.8.4, 1.9.3, 1.10.2, 1.11.0, 1.12.rc1, 1.13.2, 1.14.0, 1.15.1, 1.16.0, 1.17alpha
-
+#
 # NOTES REGARDING CHANGING TO PERL PACKAGE AND ADDING API SUPPORT
 # - constructor:
 #   - login
