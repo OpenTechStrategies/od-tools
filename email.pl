142a13870b5ab704bc96d8460bf7d593d76b3f7c
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-03 03:35:57 +0000

    start script for changing a wiki to GUID-only records
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@367 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- email.pl
+++ email.pl
@@ -10,119 +10,66 @@ use Net::POP3;
 sub emailGetMessages {
 	my %args = (@_);
 	my $server;
+	my $maxsize = 4096;
+	my @messages = ();
 
-	# Connect to POP3 server and login
+	# POP3
 	if ( $args{proto} eq 'POP3' ) {
+
+		# Connect & login
 		$server = Net::POP3->new( $args{host} );
 		$server->login( $args{user}, $args{pass} );
+		
+		# Process messages
+		my @list = keys %{ $server->list() };
+		for ( @list ) {
+			my $content = join "\n", @{ $server->top( $_, $maxsize ) };
+			push @messages, emailProcessMessage( $content, \@args );
+		}
+		
+		# Close
+		$server->quit();
 	}
 
-	# Connect to IMAP server and login
-	elsif ( $args{proto} eq 'IMAP' ) {	
+	# IMAP
+	elsif ( $args{proto} eq 'IMAP' ) {
+		
+		# Connect & login
 		$server = new Net::IMAP::Simple::SSL( $args{host} );
 		$server->login( $args{user}, $args{pass} );
-		$number_of_messages = $server->select( $args{path} or 'Inbox' );
+		
+		# Process messages
+		$count = $server->select( $args{path} or 'Inbox' );
+		for ( 1 .. $count ) {
+			$fh = $server->getfh( $_ );
+			sysread $fh, ( my $content ), $maxsize;
+			close $fh;
+			push @messages, emailProcessMessage( $content, \@args );
+		}
+
+		# Close
+		$server->quit();
 	}
 	
 	else { die "Unsupported email protocol!" }
 
+	return @messages;
+}
 
+# Returns passed message as an array reference if its attributes match the rules in @args
+sub emailProcessMessage {
+	my $content = shift;
+	my @args    = @{ shift };
+	my @return  = undef;
+	my $to      = $1 if $content =~ /^to:\s*(.+?)\s*$/mi;
+	my $from    = $1 if $content =~ /^from:\s*(.+?)\s*$/mi;
+	my $subject = $1 if $content =~ /^subject:\s*(.+?)\s*$/im;
 
-
-		
-	#pop3 message loop
-		my $list = $pop3->list();
-		my @messages = keys %$list;		
-
-
-
-	# imap message loop
-	foreach $msg ( 1..5 ) {
-
-		if ( $server->seen( $msg ) ) {
-			print "This message has been read before...\n"
-		}
-
-		# get the message, returned as a reference to an array of lines
-		$lines = $server->get( $msg );
-
-		# print it
-		print @$lines;
-
-		# get the message, returned as a temporary file handle
-		$fh = $server->getfh( $msg );
-		print <$fh>;
-		close $fh;
-
+	# Test message against @args regex's
+	if ( matches ) {
+		@return = \( $from, $to, $subject, $content );
 	}
 
-	# the list of all folders
-	@folders = $server->mailboxes();
-#	print "folders: @folders\n";
+	return @return;
+}
 
-	# create a folder
-	$server->create_mailbox( 'Contacts.newfolder' );
-
-	# rename a folder
-#	$server->rename_mailbox( 'newfolder', 'renamedfolder' );
-
-	# delete a folder
-#	$server->delete_mailbox( 'renamedfolder' );
-
-	# copy a message to another folder
-   print  $server->copy( 1, 'Contacts' );
-
-	# close the connection
-	$server->quit();
-
-
-###########
-
-
-sub popCheck {
-
-
-	# Loop through all messages
-
-	logAdd( ($#messages+1)." messages on $user\@$domain" );
-	for ( @messages ) {
-
-		# Get relevent message headers and content
-		my $msg = $pop3->top( $_, 4096 );
-		chomp @$msg;
-		( my $to ) = grep /^to:/i, @$msg;
-		if ( $to =~ /([-.&a-z0-9_]+)@([-.a-z0-9_]+)/i ) {
-
-			( my $page, my $domain ) = ( $1, $2 );
-			$to = "$page\@$domain";
-			$page =~ tr/&/%/;
-			( my $from ) = grep /^from:/i, @$msg;
-			$from = $1 if $from =~ /([-.&a-z0-9_]+@[-&.a-z0-9_]+)/i;
-			( my $subject ) = grep /^subject:/i, @$msg;
-			$subject =~ s/^.+?:\s*(.*?)(\r?\n)*/$1/;
-
-			# Couldn't forward an od addr to another od addr at zoneedit
-			$page = 'user_talk%3ajewel' if $to =~ /^jewel@/;
-
-			# Append if addressed to a talk page
-			if ( $page =~ /^(\w*?_)?talk%3a/i ) {
-				$) while shift @$msg;
-				my $append = "\n----\n'''Email from [mailto:$from $from]''', ~~";
-				$append .= "~~\n\n'''''$subject'''''\n:".join( "\n:", @$msg )."\n";
-				$loggedIn = wikiLogin $::wiki, $::peer, $::pwd1 unless $loggedIn;
-				wikiPageAppend $::wiki, $page, $append, $subject;
-				$pop3->delete($_);
-				}
-
-			# Execute if an email command
-			if ( $subject =~ /^(.+?):(.+?)\((.*?)\)\s*(.*)$/ and $1 eq $::pwd5 ) {
-				spawn $2, split /\s*,\s*/, $3;
-				logAdd "Executing $2";
-				$pop3->delete($_);
-				}
-
-			}
-		}
-	$pop3->quit();
-	logAdd 'Exit';
-	}
