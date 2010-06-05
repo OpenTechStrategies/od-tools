b44c6e47576cc794596edf2e6c5e94ab57ef58ab
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-05 03:24:48 +0000

    Add GPL to jobs scripts
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@372 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- jobs/ModifyRecords.pl
+++ jobs/ModifyRecords.pl
@@ -1,8 +1,23 @@
 #!/usr/bin/perl
 #
-# Subroutines for ModifyRecords job called by wikid.pl (Organic Design wiki daemon)
+# Subroutines for "ModifyRecords" job called by wikid.pl (Organic Design wiki daemon)
 #
-# @author Aran Dunkley http://www.organicdesign.co.nz/nad
+# Copyright (C) 2009-2010 Aran Dunkley and others.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# http://www.gnu.org/copyleft/gpl.html
 #
 
 # Set the file pointer to start of the file (i.e. first line)
