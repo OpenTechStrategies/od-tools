94691d0b011cb554aa40d052da39b4df7ce720bd
Aran Dunkley <aran@organicdesign.co.nz>
2010-08-30 08:33:09 +0000

    general structure of site code
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@429 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- Sandy/mtweb.php
+++ Sandy/mtweb.php
@@ -8,5 +8,81 @@
  * Version 1.0 started on 2010-08-30
  */
 
+$version = '1.0.0 (2010-08-30)';
+$maxage  = 900;
+
+switch( $_GET['action'] ) {
+
+	case 'register':
+	
+		# Registration page
+		?><html>
+			<head></head>
+			<body>
+				This is the home page...
+			</body>
+		</html><?php
+
+	break;
+
+	case 'comfirm':
+	
+		# Email confirmation
+		?><html>
+			<head></head>
+			<body>
+				This is the home page...
+			</body>
+		</html><?php
+
+	break;
+
+	case 'payment':
+	
+		# Payment page
+		?><html>
+			<head></head>
+			<body>
+				This is the home page...
+			</body>
+		</html><?php
+	
+	break;
+
+	case 'api':
+	
+		# Connection from an mtconnect.exe instance
+		$items = '';
+		
+		# - check if key valid and current
+		$key = $_GET['key'];
+		if( !$valid )  die( "Error 1: supplied key is invalid." );
+		if( $expired ) die( "Error 2: supplied key has expired." );
+
+		# return items since last
+		if( $last = $_GET['last'] ) {
+
+			# Get the items since the last one
+
+		} else {
+
+			# Get the items newer than $maxage
+
+		}
+		
+		print $items;
+	
+	break;
+
+	default:
+	
+		# Home page
+		?><html>
+			<head></head>
+			<body>
+				This is the home page...
+			</body>
+		</html><?php
+}
 
 ?>
