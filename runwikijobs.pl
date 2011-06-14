2eb2a36cae010d29dc249c543fc6223a51283646
Aran Dunkley <aran@organicdesign.co.nz>
2011-06-14 08:23:47 +0000

    new script to loop through all wikis requesting login page
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@522 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

---
+++ runwikijobs.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# Synchronise an article from one wiki to all others in the DCS wikia
+#
+# Author: http://www.organicdesign.co.nz/nad
+#
+
+# Set defaults
+$domain = "debtcompliance.com";
+$lsfile = "/var/www/wikis/dc2/LocalSettings.php";
+
+# Read in the LocalSettings.php file for the DCS wikia
+open LS, '<', $lsfile or die "Could not read LocalSettings.php for DCS wikia!";
+sysread LS, $ls, -s $lsfile;
+close LS;
+
+# Extract a list of wikis from it
+@wikis = ( $ls =~ /^\s*\$wgShortName\s*=\s*"(.+?)";\s*$/gm );
+
+# Read the content of the source article
+$text = wikiRawPage( $srcwiki, $title );
+
+# Set up a global client for making HTTP requests as a browser
+$ua = LWP::UserAgent->new(
+	cookie_jar => {},
+	agent      => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.14)',
+	from       => 'runwikijobs.pl@organicdesign.co.nz',
+	timeout    => 10,
+	max_size   => 1024
+);
+
+# Loop through all wikis requesting the main page of each
+$ua->get( "https://$_.$domain/wiki/index.php?title=Special:Userlogin" ) for @wikis;
+
+0;
