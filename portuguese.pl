72453a938c9332b467dd0911ea47225b951e082d
Aran Dunkley <aran@organicdesign.co.nz>
2011-08-10 02:16:02 +0000

    get images to show in the message box if one is associated with the sentence
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@533 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- portuguese.pl
+++ portuguese.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -w
 use Tk;
 use Tk::DialogBox;
+use Tk::widgets qw/JPEG PNG/; # perl-tk
+use Image::Info qw(image_info dim);   # libimage-info-perl
 use Encode qw(encode decode);
+
+$lessons = "/home/nad/Contacts/Beth/Lessons";
 
 # Time to wait before displaying lesson
 $t = int( 60 + rand( 600 ) );
@@ -13,8 +17,19 @@ $t = int( 60 + rand( 600 ) );
 exit 0 if qx( ps aux | grep portuguese-running | grep -v grep );
 
 # Read the list from the file
-open FH, '<', "$0.txt";
-push @lessons, $_ for grep /^.+/, <FH>;
+open FH, '<', "$lessons/sentences.txt";
+$i = 0;
+@lessons = ();
+for( <FH> ) {
+	chomp;
+	if( /^.+$/ ) {
+		push @lessons, $_;
+		$i++;
+	} else {
+		push @lessons, "" if $i < 3;
+		$i = 0;
+	}
+}
 close FH;
 
 # Wait for a random time
@@ -22,21 +37,35 @@ $0 = "portuguese-running ($t seconds)";
 sleep( $t );
 
 # Pick a lesson from the list
-$n = int( rand( 0.5 + $#lessons / 2 ) ) * 2;
-( $q, $a ) = rand() < 0.5 ? ( 0, 1 ) : ( 1, 0 );
+$n = int( rand( 0.5 + $#lessons / 3 ) ) * 3;
 
-$d = MainWindow->new;
-$d->withdraw();
+# Set up tk main window
+$mw = MainWindow->new;
+$mw->withdraw();
+
+# Load image if any
+if( $img = $lessons[$n+2] ) {
+	$file = "$lessons/Pictures/$img";
+	($w, $k) = dim( image_info( $file ) );
+	$k = $w / 150;
+	$image = $mw->Photo( -file => $file );
+	$resized = $mw->Photo( 'resized' );
+	$resized->copy( $image, -subsample => $k, $k );
+	( $q, $a ) = ( 1, 0 );
+} else { ( $q, $a ) = rand() < 0.5 ? ( 0, 1 ) : ( 1, 0 ) }
+
 
 # Display the question dialog
-$d->messageBox(
+%args = (
 	-title   => "Question",
 	-message => decode( "UTF-8", $lessons[$n + $q] ),
 	-buttons => [ "Ok" ]
 );
+$args{-image} = $resized if defined $resized;
+$mw->messageBox( %args );
 
 # Display the answre dialog
-$d->messageBox(
+$mw->messageBox(
 	-title   => "Answer",
 	-message => decode( "UTF-8", $lessons[$n + $a] ),
 	-buttons => [ "Ok" ]
