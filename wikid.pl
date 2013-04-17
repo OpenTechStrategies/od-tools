c9afbbe83f0f1a5f131517358491585f15c6be42
Aran Dunkley <aran@organicdesign.co.nz>
2013-04-17 23:35:45 +0000

    filter svn user from irc notify
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@654 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- wikid.pl
+++ wikid.pl
@@ -50,7 +50,7 @@ $daemon   = 'wikid';
 $host     = uc( hostname );
 $name     = hostname;
 $port     = 1729;
-$ver      = '3.19.6'; # 2011-03-10
+$ver      = '3.19.7'; # 2013-04-17
 $log      = "$dir/$daemon.log";
 $wkfile   = "$dir/$daemon.work";
 
@@ -607,7 +607,7 @@ sub onFileChanged {
 	my $newsize = shift;
 	my $text    = '';
 	my $msg     = '';
-	my @userfilter = ( $::netuser, 'root', 'nobody', 'fit', 'munin' );
+	my @userfilter = ( $::netuser, 'root', 'nobody', 'fit', 'munin', 'svn' );
 
 	# Read in difference
 	if ( $newsize > $oldsize and open FH, '<', $file ) {
