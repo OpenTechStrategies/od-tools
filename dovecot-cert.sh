fcb12ed7a8a6ffb304fd925a3b1818e1371808f6
Aran Dunkley <aran@organicdesign.co.nz>
2013-06-27 14:23:49 +0000

    use dovecot default location
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@658 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- dovecot-cert.sh
+++ dovecot-cert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-openssl genrsa -out /etc/ssl/private/dovecot.pem 1024
-openssl req -new -key /etc/ssl/private/dovecot.pem -out /tmp/csr
-openssl x509 -in /tmp/csr -out /etc/ssl/certs/dovecot.pem -req -signkey /etc/ssl/private/dovecot.pem -days 999
+openssl genrsa -out /etc/dovecot/private/dovecot.pem 1024
+openssl req -new -key /etc/dovecot/private/dovecot.pem -out /tmp/csr
+openssl x509 -in /tmp/csr -out /etc/dovecot/dovecot.pem -req -signkey /etc/dovecot/private/dovecot.pem -days 999
 
