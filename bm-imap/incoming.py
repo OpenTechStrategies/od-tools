050e24f66e6ca34cbf25b67da40401b6c37492b0
Aran Dunkley <aran@organicdesign.co.nz>
2013-08-18 18:15:40 +0000

    import ConfigParser
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@689 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- bm-imap/incoming.py
+++ bm-imap/incoming.py
@@ -7,7 +7,7 @@ from incoming import *
 # This script is now called directly on a cronjob and sends the retrieved Bitmessage messages to a local email address
 config = ConfigParser.SafeConfigParser()
 config.read('~/.config/PyBitmessage/keys.dat')
-emailAddresses = config.items('Email addresses')
+emailUsers = config.items('Email users')
 
 # Loop through the Bitmessage messages
 msgCount = bminterface.listMsgs()
