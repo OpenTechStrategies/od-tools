f76b19df7f54c73531216bbb59757c73ab5411c4
Aran Dunkley <aran@organicdesign.co.nz>
2013-08-18 17:01:58 +0000

    extract BM address
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@677 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- bm-imap/incoming.py
+++ bm-imap/incoming.py
@@ -245,6 +245,9 @@ def incomingServer_main(host, port):
         sock.close()
 
 # This script is now called directly on a cronjob and sends the retrieved Bitmessage messages to a local email address
+config = ConfigParser.SafeConfigParser()
+config.read(keysPath)
+emailAddresses = config.items('Email addresses')
 
 # Loop through the Bitmessage messages
 msgCount = bminterface.listMsgs()
@@ -252,8 +255,12 @@ print "%i messages to parse" % (msgCount)
 for msgID in range(msgCount):
 	print "Parsing msg %i" % (msgID+1)
 
-	# Get the message and make into an email
+	# Get the Bitmessage message
 	dateTime, toAddress, fromAddress, subject, body = bminterface.get(msgID)
+
+	# Convert the To and From addresses to raw Bitmessage addresses
+	print re.match(r'^(.+)@', toAddress).group(1)
+
 	msg = makeEmail(dateTime, toAddress, fromAddress, subject, body)
 
 	# Send the message to the local address
