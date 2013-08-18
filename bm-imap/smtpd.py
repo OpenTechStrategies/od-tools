e7cf4e12e69e0e4ff0ff9531b4075224a99dbcdf
Aran Dunkley <aran@organicdesign.co.nz>
2013-08-18 23:51:27 +0000

    outgoing - try a smtpd dummy class first
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@722 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- bm-imap/smtpd.py
+++ bm-imap/smtpd.py
@@ -1,3 +1,5 @@
+# This is a dummy class so that we can instantiate an instance of
+# outgoing.py to call its methods without starting up an SMTP server
 class SMTPServer:
-	def __init__(self)
+	def __init__(self):
 		print "dummy class"
