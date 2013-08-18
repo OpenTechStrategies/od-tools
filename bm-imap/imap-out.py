8b5cbba77f1640ed2412a8fb0d44466b1e877bbc
Aran Dunkley <aran@organicdesign.co.nz>
2013-08-18 23:47:55 +0000

    outgoing - try a smtpd dummy class first
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@720 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- bm-imap/imap-out.py
+++ bm-imap/imap-out.py
@@ -1,113 +1,30 @@
 #!/usr/bin/python2.7
-import asyncore
-from smtpd import SMTPServer
+import os
+import sys
+import re
 import email.parser, email.header
-import bminterface
 
-class outgoingServer(SMTPServer):
-    def process_message(self, peer, mailfrom, rcpttos, data):
-        parser = email.parser.FeedParser()
-        parser.feed(data)
-        msg = parser.close()
+# Get username and home dir
+currentUser = os.getlogin()
+currentUser = 'odsmtp'
 
-        toAddress = msg['To']
-        fromAddress = msg['From']
-        subject = u' '.join(unicode(t[0], t[1] or 'UTF-8') for t in email.header.decode_header(msg['Subject'])).encode('UTF-8')
-        body = self._bmformat(msg)
-        
-        #Make sure we don't send an actually blank subject or body--this can cause problems.
-        if not subject:
-          subject = ' '
-        if not body:
-          body = ' '
-        
-        if bminterface.send(toAddress, fromAddress, subject, body):
-          print "Message queued for sending..."
-        else:
-          print "There was an error trying to send the message..."
-        
-        return 0
-    
-    def _bmformat(self, msg):
-      disclaimer = "\n<!-- Email sent from bmwrapper -->\n<!-- https://github.com/Arceliar/bmwrapper -->\n"
-      imageNotice = "<!-- Note: An image is attached below -->\n"
-      if not msg.is_multipart():
-        #This is a single part message, so there's nothing to do.
-        #Will still parse, just to get rid of awkward quote '>' everywhere.
-        myText, oldText = self._parseQuoteText(msg.get_payload())
-        return myText + oldText
-      else:
-        #This is a multipart message.
-        #Unfortunately, now we have to actually do work.
-        myText, oldText, image = self._recurseParse(msg)
-        return imageNotice + myText +  oldText + disclaimer + image
-      
-    def _recurseParse(self, msg):
-      text = ''
-      image = ''
-      for item in msg.get_payload():
-        if 'text/plain' in item['Content-Type']:
-          text += item.get_payload()
-        elif 'image' in item['Content-Type']:
-          [filetype, name] = item['Content-Type'].rstrip().split('\n')
-          name = name.replace('name', 'alt')
-          imageraw = item.get_payload().rstrip().split('\n')
-          imagedata = ''
-          for line in imageraw:
-            if not line[0] == '-':
-              imagedata += line
-          image += '<img '+name+' src="data:'+filetype+'base64,'+imagedata+'" />'
-        elif 'multipart' in item['Content-Type']:
-          firstTest_new, text_new, image_new = self._recurseParse(item)
-          text += firstText_new + text_new
-          image += image_new
-        else:
-          #Note that there's a chance we may die horribly if nothing returns.
-          pass
-      firstText, text = self._parseQuoteText(text)
-      return firstText, text, image
+# Import modules from bmwrapper
+sys.path.append( '/home/' + currentUser + '/bmwrapper' )
+from bminterface import *
+from outgoing import *
 
-    def _parseQuoteText(self, text):
-      rawText = text.split('\n')
-      tempText = []
-      text = ''
-      firstText = ''
-      n = 0
-      while len(rawText):
-        for line in range(len(rawText)):
-          if rawText[line]:
-            if (rawText[line][0] == '>'): # and rawText[line].strip('>')[0] == ' '):
-              rawText[line] = rawText[line][1:]
-              if rawText[line]:
-                if rawText[line][0] == ' ':
-                  rawText[line] = rawText[line][1:]
-              tempText.append(rawText[line])
-            else:
-              if n == 0:
-                firstText += rawText[line]+'\n'
-              else:
-                text += rawText[line]+'\n'
-          else:
-            if n == 0:
-              firstText += '\n'
-            else:
-              text += '\n'
-        if len(tempText):
-          text += '\n\n------------------------------------------------------\n'
-        rawText = tempText
-        tempText = []
-        n += 1
-      text = text.rstrip('\n')
-      firstText = firstText.rstrip('\n')
-      return firstText, text
-        
-def run():
-    foo = outgoingServer(('localhost', 12345), None)
-    try:
-        asyncore.loop()
-    except KeyboardInterrupt:
-        pass
+# The email is passed to STDIN
+#my @input = <STDIN>;
 
+# The URL to post the extracted data to is a program argument
+#my $post = $ARGV[0];
 
-if __name__ == '__main__':
-	run()
+# Extract the useful header portion of the message
+#my $id      = $1 if $email =~ /^message-id:\s*<(.+?)>\s*$/mi;
+#my $date    = $1 if $email =~ /^date:\s*(.+?)\s*$/mi;
+#my $to      = $1 if $email =~ /^to:\s*(.+?)\s*$/mi;
+#my $from    = $1 if $email =~ /^from:\s*(.+?)\s*$/mi;
+#my $subject = $1 if $email =~ /^subject:\s*(.+?)\s*$/im;
+
+foo = outgoingServer()
+
