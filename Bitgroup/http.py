7f49ceb3f19270ab6fe12539b8d24bdd084360e7
Aran Dunkley <aran@organicdesign.co.nz>
2013-09-02 19:23:21 +0000

    return test http message in response to incoming connections
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@805 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- Bitgroup/http.py
+++ Bitgroup/http.py
@@ -2,26 +2,28 @@
 # it must populate and server the html templates and serve the JS, CSS and image resources
 import socket
 import asyncore
+import time
 
 class handler(asyncore.dispatcher_with_send):
 
     def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            self.send(data)
+		data = self.recv(8192)
+		if data:
+			date = time.strftime("%a, %d %b %Y %H:%M:%S %Z")
+			self.send("HTTP/1.0 200 OK\r\nDate: " + date + "\r\nServer: Bitgroup\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: 6\r\n\r\nHello!")
 
 class server(asyncore.dispatcher):
 
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(5)
+	def __init__(self, host, port):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((host, port))
+		self.listen(5)
 
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            print 'Incoming connection from %s' % repr(addr)
-            h = handler(sock)
+	def handle_accept(self):
+		pair = self.accept()
+		if pair is not None:
+			sock, addr = pair
+			print 'Incoming connection from %s' % repr(addr)
+			h = handler(sock)
