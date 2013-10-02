0fb84612f3539e3e5d13b40a177669d799c3b0dd
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-02 19:08:51 +0000

    debug filter
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@940 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- mark-as-read.pl
+++ mark-as-read.pl
@@ -1,2 +1,10 @@
 #!/usr/bin/perl
+
+my $file = '/home/nad/eximtest';
+if( open FH,'>>', $file ) {
+	print FH $ENV{HOME} . "\n";
+	print FH $ENV{USER} . "\n";
+	print FH $ENV{MESSAGE_ID} . "\n\n";
+	close FH;
+
 for(glob "/home/*/Maildir/.Sent/cur/*") { rename $_, $_.'S' if /2,$/ }
