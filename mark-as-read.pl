31931b0dc0dd21a02407cdc40f719624f6a30ffb
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-02 20:52:26 +0000

    must check id - log better
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@955 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- mark-as-read.pl
+++ mark-as-read.pl
@@ -11,16 +11,20 @@ if( open FH,'<', $file ) {
 		my $file = "/home/nad/eximtest";
 		if( open FH,'>', $file ) {
 			print FH "user: $user\nid: $id\n";
-			close FH;
-		}
 
-		$id = $ENV;
-		for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
-			if( open FMSG,'<', $msg ) {
-				sysread FMSG, $content, -s $msg;
-				close FMSG;
-				rename $msg, "$msg:2,S" if $content =~ /^\s*id\s*$id\s*$/m;
+			$id = $ENV;
+			for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
+				print FH "$msg\n";
+				if( open FMSG,'<', $msg ) {
+					sysread FMSG, $content, -s $msg;
+					close FMSG;
+					if( $content =~ /^\s*id\s*$id\s*$/m ) {
+						print FH "\tmatch\n";
+						rename $msg, "$msg:2,S";
+					}
+				}
 			}
+			close FH;
 		}
 	}
 }
