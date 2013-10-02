94671203ff08862de09d862c869412c1238a2df9
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-02 19:22:17 +0000

    delay before setting dir to read
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@944 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- mark-as-read.pl
+++ mark-as-read.pl
@@ -1,11 +1,3 @@
 #!/usr/bin/perl
-
-my $file = '/home/nad/eximtest';
-if( open FH,'>>', $file ) {
-	print FH $ENV{SENDER} . "\n";
-	print FH getpwuid($<) . "\n";
-	print FH $ENV{MESSAGE_ID} . "\n\n";
-	close FH;
-}
-
+sleep(0.5);
 for(glob "/home/*/Maildir/.Sent/cur/*") { rename $_, $_.'S' if /2,$/ }
