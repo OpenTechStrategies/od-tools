b7ba0cf0c3075457a6753ff582c2786faaeef95d
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-02 20:19:55 +0000

    try adding seen in new
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@947 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- mark-as-read.pl
+++ mark-as-read.pl
@@ -1,2 +1,11 @@
 #!/usr/bin/perl
-for(glob "/home/*/Maildir/.Sent/new/*") { rename $_, $_.':2,S' unless /:/ }
+
+my $file = "/home/nad/eximtest";
+if ( open FH,'>', $file ) {
+	print FH $ENV{LOGNAME};
+	close FH;
+}
+
+for(glob "/home/*/Maildir/.Sent/new/*") {
+	rename $_, $_.':2,S' unless /:/
+}
