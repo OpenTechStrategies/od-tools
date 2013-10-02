86affecfb66778a3376c7aa0c5277a83246e1754
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-02 21:00:22 +0000

    must check id - rm debugging
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@957 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- mark-as-read.pl
+++ mark-as-read.pl
@@ -7,24 +7,16 @@ if( open FH,'<', $file ) {
 	close FH;
 	if( $users =~ /^$sender\s*:\s*(.+?)\@localhost\s*$/m ) {
 		$user = $1;
-
-		my $file = "/home/nad/eximtest";
-		if( open FH,'>', $file ) {
-			print FH "user: $user\nid: $id\n";
-
-			$id = $ENV;
-			for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
-				print FH "$msg\n";
-				if( open FMSG,'<', $msg ) {
-					sysread FMSG, $content, 600;
-					close FMSG;
-					if( $content =~ /\s$id\s/s ) {
-						print FH "\tmatch\n";
-						rename $msg, "$msg:2,S";
-					}
+		$id = $ENV;
+		for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
+			print FH "$msg\n";
+			if( open FMSG,'<', $msg ) {
+				sysread FMSG, $content, 600;
+				close FMSG;
+				if( $content =~ /\s$id\s/s ) {
+					rename $msg, "$msg:2,S";
 				}
 			}
-			close FH;
 		}
 	}
 }
