34fc5305f1f7c7c97ba898ac9f98fd0d5e16fcf1
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-28 15:20:53 +0000

    to and cc can span multiple lines
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1012 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- copy-to-sent.pl
+++ copy-to-sent.pl
@@ -29,10 +29,10 @@ if( open FH,'<', $file ) {
 					close FMSG;
 
 					# Turn the To and CC headers into lists and then hashes
-					$to = $1 if $content =~ /^\s*To:\s*(.+?)\s*$/mi;
+					$to = $content =~ /^\s*To:\s*(.+?)\s+(\w: )/mis ? $1 : '';
 					@to = $to =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
 					%to = map { $_ => 1 } @to;
-					$cc = $1 if $content =~ /^\s*CC:\s*(.+?)\s*$/mi;
+					$cc = $content =~ /^\s*CC:\s*(.+?)\s+(\w: )/mis ? $1 : '';
 					@cc = $cc =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
 					%cc = map { $_ => 1 } @cc;
 
