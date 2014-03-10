4e427892588f182833d970e04a3caae2cce068ff
Aran Dunkley <aran@organicdesign.co.nz>
2014-03-10 12:00:03 +0000

    comments
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1024 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- rotate.pl
+++ rotate.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
 $dev = 'Atmel Atmel maXTouch Digitizer';
-$matrix = qx( xinput list-props '$dev'|grep 'Coordinate Transformation Matrix' );
+$prop = 'Coordinate Transformation Matrix';
+
+# Get the current matrix
+$matrix = qx( xinput list-props '$dev'|grep '$prop' );
 $matrix =~ s/^.+?:\s*//gs;
 $matrix =~ s/\.\d+,?//gs;
 chomp $matrix;
+
+# Get the new value for the display and input
 if( $matrix eq '1 0 0 0 1 0 0 0 1' ) {
 	$matrix = '0 1 0 -1 0 1 0 0 1' ;
 	$rotate = 'right';
-}
-else {
+} else {
 	$matrix = '1 0 0 0 1 0 0 0 1' ;
 	$rotate = 'normal';
 }
+
+# Commit the changes to the devices
 qx( xrandr -o $rotate );
-qx( xinput set-prop '$dev' 'Coordinate Transformation Matrix' $matrix );
+qx( xinput set-prop '$dev' '$prop' $matrix );
 
