95cff2a26700eafead29f4ca530787a979752724
Aran Dunkley <aran@organicdesign.co.nz>
2014-04-05 14:32:57 +0000

    change to left rotation and add the wacom stylus
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1025 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- rotate.pl
+++ rotate.pl
@@ -10,14 +10,17 @@ chomp $matrix;
 
 # Get the new value for the display and input
 if( $matrix eq '1 0 0 0 1 0 0 0 1' ) {
-	$matrix = '0 1 0 -1 0 1 0 0 1' ;
-	$rotate = 'right';
+	$matrix = '0 -1 1 1 0 0 0 0 1' ;
+	$rotate = 'left';
+	$wacom = 'ccw';
 } else {
 	$matrix = '1 0 0 0 1 0 0 0 1' ;
 	$rotate = 'normal';
+	$wacom = 'none';
 }
 
 # Commit the changes to the devices
 qx( xrandr -o $rotate );
 qx( xinput set-prop '$dev' '$prop' $matrix );
-
+qx( xsetwacom set "Wacom ISDv4 EC Pen stylus" rotate $wacom );
+qx( xsetwacom set "Wacom ISDv4 EC Pen eraser" rotate $wacom );
