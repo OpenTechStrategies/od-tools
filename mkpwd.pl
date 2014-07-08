b5761aa9f23df7bc90498119a06d546380272d62
Aran Dunkley <aran@organicdesign.co.nz>
2014-07-08 20:03:56 +0000

    use this for making pwds
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1026 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

---
+++ mkpwd.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+sub rnd { return int(rand()*shift) }
+$a = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$s = '~!@#$%^&*()-_+={}[]:;/?.,<>|';
+for( 1 .. ($ARGV[0] ? $ARGV[0] : 1) ) {
+	$pwd = '';
+	$pwd .= substr($a,rnd(length $a),1) for (0..rnd(3));
+	$pwd .= substr($s,rnd(length $s),1);
+	$pwd .= substr($a,rnd(length $a),1) for (1..rnd(3));
+	$pwd .= substr($s,rnd(length $s),1);
+	$pwd .= substr($a,rnd(length $a),1) for (1..rnd(3));
+	$pwd .= substr($s,rnd(length $s),1);
+	$pwd .= substr($a,rnd(length $a),1) while length $pwd < 10;
+	print "$pwd\n" ;
+}
+
