7836abdc048de094fa07753d46865346c6a9a2c0
Aran Dunkley <aran@organicdesign.co.nz>
2014-07-26 14:42:55 +0000

    print dir
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1033 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- learn-spam.pl
+++ learn-spam.pl
@@ -3,13 +3,13 @@
 # Handle false positives
 for (glob "/home/*/Maildir/.INBOX.Not\\ Spam/[cn]??") {
 	s/ /\\ /;
-	print qx "sa-learn --ham $_";
+	print "$_: " . ( qx "sa-learn --ham $_" );
 	qx "rm -fr $_/*";
 }
 
 # Handle false negatives
 for (glob "/home/*/Maildir/.INBOX.Spam/[cn]??") {
 	s/ /\\ /;
-	print qx "sa-learn --spam $_";
+	print "$_: " . ( qx "sa-learn --spam $_" );
 	qx "rm -fr $_/*";
 }
