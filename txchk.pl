35fb878833e421732963a13c03c0637707a9ca21
Aran Dunkley <aran@organicdesign.co.nz>
2014-09-09 17:44:54 +0000

    tidy up
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1051 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- txchk.pl
+++ txchk.pl
@@ -34,33 +34,34 @@ if(-e "txchk.hist") {
 	close FH;
 }
 
-# Loop through each address in the config
+# Loop through each line in the config
 $changed = 0;
 open FH, '<', "txchk.conf";
 while(<FH>) {
 
-	# Extract the bitcoin and email address from the current line
-	if(/^\s*(.+)\s+(\S+)\s*$/) {
+	# If the line has a bitcoin and email address,
+	if( /^\s*(.+)\s+(\S+)\s*$/ ) {
+
+		# Get the info from the config line
 		($addr,$email) = ($1,$2);
 
-		# Get the current bitcoin balance of the address from blockchain.info
+		# Get the current bitcoin balance for the address from blockchain.info
 		$raw = $addr =~ /^(\w+)/ ? $1 : $addr;
 		$bal = $ua->get( "https://blockchain.info/q/addressbalance/$raw" )->content / 100000000;
 
-		# If it's more than the last amount, compose a message
+		# If it's more than the last recorded balance,
 		if( $bal > $hist{$addr} ) {
-			$changed = 1;
 
-			# Get the transaction amount
+			# Get the transaction amount and update the recorded balance
 			$tx = $bal - $hist{$addr};
 			$hist{$addr} = $bal;
 			$txd = dollar( $tx * $btc );
 			$tx =~ s/(\d{8})\d+/$1/;		
 
-			# Compose the message
+			# Compose a message
 			$msg = "You received $tx BTC (\$$txd) to address $addr\n\nThe current bitcoin price is \$" . dollar($btc) . " USD";
 
-			# Mail the info
+			# Send the info to the email address
 			$tmp = "/tmp/mail.txt";
 			open MSG,'>', $tmp;
 			print MSG $msg;
@@ -68,12 +69,14 @@ while(<FH>) {
 			$email =~ s/@/\\@/;
 			qx( mail -s "You received $tx BTC" $email < $tmp );
 			qx( rm -f $tmp );
+
+			$changed = 1;
 		}
 
-		# If the balance has got less, update the hist value
+		# If the balance has got less, update the recorded balance
 		elsif( $bal < $hist{$addr} ) {
-			$changed = 1;
 			$hist{$addr} = $bal;
+			$changed = 1;
 		}
 	}
 }
