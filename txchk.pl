3bdb5f5eeb791aab0143fe8a2708db7011d7bd01
Aran Dunkley <aran@organicdesign.co.nz>
2014-09-09 20:00:07 +0000

    make it work with actual transaction IDs not just balance
    git-svn-id: svn+ssh://organicdesign.co.nz/tools@1052 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- txchk.pl
+++ txchk.pl
@@ -4,7 +4,9 @@
 #
 use HTTP::Request;
 use LWP::UserAgent;
-use Cwd qw(realpath);
+use JSON qw( decode_json );
+use Cwd qw( realpath );
+use Data::Dumper;
 
 # Set up a client for making HTTP requests and don't bother verifying SSL certs
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
@@ -13,20 +15,11 @@ $ua = LWP::UserAgent->new();
 # Change to the directory the code's in
 chdir $1 if realpath($0) =~ m|^(.+)/|;
 
-# Sub to format numbers as 2dp with commas
-sub dollar {
-	my $x = (shift) + 0.0001;
-	$x =~ s/^(.+?\...).+/$1/;
-	$x =~ s/(\d)(?=\d\d\d\.)/$1,/;
-	$x =~ s/(\d)(?=\d\d\d,)/$1,/;
-	return $x;
-}
-
 # Get the current bitcoin price from blockchain.info
 $src = $ua->get( "http://blockchain.info/ticker" )->content;
 $btc = $src =~ /USD.+?15m.+?([0-9.]{3,})/ ? $1 : 'ERROR';
 
-# Load the last balance data
+# Load the last historical data containing the last txid for each address
 %hist = ();
 if(-e "txchk.hist") {
 	open FH, '<', "txchk.hist";
@@ -45,38 +38,37 @@ while(<FH>) {
 		# Get the info from the config line
 		($addr,$email) = ($1,$2);
 
-		# Get the current bitcoin balance for the address from blockchain.info
+		# get all transactions to this address that have occurred since the last recorded one
 		$raw = $addr =~ /^(\w+)/ ? $1 : $addr;
-		$bal = $ua->get( "https://blockchain.info/q/addressbalance/$raw" )->content / 100000000;
+		@txs = get_tx_list( $raw, $hist{$raw} );
 
-		# If it's more than the last recorded balance,
-		if( $bal > $hist{$addr} ) {
+		# Don't do anything if there's no transactions in the list
+		if( $#txs >= 0 ) {
 
-			# Get the transaction amount and update the recorded balance
-			$tx = $bal - $hist{$addr};
-			$hist{$addr} = $bal;
-			$txd = dollar( $tx * $btc );
-			$tx =~ s/(\d{8})\d+/$1/;		
+			# Update the last txid if its different and mark history as changed
+			$changed = 1 if $hist{$raw} ne $txs[0];
+			$hist{$raw} = $txs[0];
 
-			# Compose a message
-			$msg = "You received $tx BTC (\$$txd) to address $addr\n\nThe current bitcoin price is \$" . dollar($btc) . " USD";
+			# Loop through all the transactions by txid
+			for my $txid ( @txs ) {
 
-			# Send the info to the email address
-			$tmp = "/tmp/mail.txt";
-			open MSG,'>', $tmp;
-			print MSG $msg;
-			close MSG;
-			$email =~ s/@/\\@/;
-			qx( mail -s "You received $tx BTC" $email < $tmp );
-			qx( rm -f $tmp );
+				# Get the amount sent to our address in this transaction
+				$tx = get_tx_info( $raw, $txid );
+				$txd = dollar( $tx * $btc );
+				$tx =~ s/(\d{8})\d+/$1/;		
 
-			$changed = 1;
-		}
+				# Compose a message about this transaction
+				$msg = "You received $tx BTC (\$$txd) to address $addr\n\nThe transaction ID is $txid\n\nThe current bitcoin price is \$" . dollar($btc) . " USD";
 
-		# If the balance has got less, update the recorded balance
-		elsif( $bal < $hist{$addr} ) {
-			$hist{$addr} = $bal;
-			$changed = 1;
+				# Send the message to the listed email address
+				$tmp = "/tmp/mail.txt";
+				open MSG,'>', $tmp;
+				print MSG $msg;
+				close MSG;
+				$email =~ s/@/\\@/;
+				qx( mail -s "You received $tx BTC" $email < $tmp );
+				qx( rm -f $tmp );
+			}
 		}
 	}
 }
@@ -87,4 +79,40 @@ if($changed) {
 	open FH, '>', "txchk.hist";
 	print FH "$_ $hist{$_}\n" for keys %hist;
 	close FH;
+}
+
+exit;
+
+
+# Sub to format numbers as 2dp with commas
+sub dollar {
+	my $x = (shift) + 0.0001;
+	$x =~ s/^(.+?\...).+/$1/;
+	$x =~ s/(\d)(?=\d\d\d\.)/$1,/;
+	$x =~ s/(\d)(?=\d\d\d,)/$1,/;
+	return $x;
+}
+
+# Get the amount received by the passed address in the passed txid
+sub get_tx_info {
+	my $addr = shift;
+	my $txid = shift;
+	my $amt = 0;
+	my $info = decode_json( $ua->get( "https://blockchain.info/rawtx/$txid" )->content );
+	for( @{$info->{'out'}} ) { $amt = $_->{value}/100000000 if $_->{addr} eq $addr };
+	return $amt;
+}
+
+# Get all transactions sent to the passed address since the passed txid
+sub get_tx_list {
+	my $addr = shift;
+	my $txid = shift;
+	my $stop = 0;
+	my @txs = ();
+	my $info = decode_json( $ua->get( "https://blockchain.info/rawaddr/$addr" )->content );
+	for( @{$info->{'txs'}} ) {
+		$stop = 1 if $_->{hash} eq $txid;
+		push @txs, $_->{hash} unless $stop;
+	}
+	return @txs;
 }
