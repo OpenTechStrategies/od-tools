b498181c38e20aa247a95a817e4a170911b9f5e5
Aran <aran@organicdesign.co.nz>
2015-05-21 16:45:12 -0300

    debugging on bigger scripts, add support for multiple sections

--- tidy.php
+++ tidy.php
@@ -28,8 +28,98 @@ class CodeTidy {
 		'<', '>', '=', '.', '+', '-', '*', '/', '%', '?', ':', '&', '|', '^',
 	);
 
+	// Operator-like things that shouldn't have space modified
+	private static $nops = array( '<?', '::', '//', '/*', '*/', '->', '=>', '@' );
+
+	/**
+	 * Main entry point, tidt the passed PHP file content
+	 */
 	public static function tidy( $code ) {
+
+		// Format the code into a uniform state ready for processing
+		self::preprocess( $code );
+
+		// Loop through all PHP sections in the content and tidy each
+		// (but not ones that are a single line since it may mess up HTML formatting)
+		$code = preg_replace_callback( "%<\?(php)?(.+?)(\?>|$)%s", function( $m ) {
+			return preg_match( "|\n|", $m[2] ) ? "<?php\n" . self::tidySection( $m[2] ) . "\n?>" : "<?php$m[2]$m[3]";
+		}, $code );
+
+		// Remove the final delimiter if any
+		$code = preg_replace( '|\s*\?>\s*$|', '', $code );
+
+		return $code;
+	}
+
+	/**
+	 * Tidy a single PHP section of code (without delimeters)
+	 */
+	private static function tidySection( $code ) {
 		self::$indent = 0;
+
+		// Move orphan opening braces to previous line
+		$code = preg_replace( '%(?<=\S)\s*\{[ \t]*$%m', ' {', $code );
+		$code = preg_replace( '%(?<=\S)\s*\{([ \t]*//)%m', ' {$1', $code );
+
+		// Put a newline after braces
+		$code = preg_replace( '%\{%m', "{\n", $code );
+
+		// Clean up brackets
+		$code = preg_replace_callback( '|\([ \t]*(.+?)[ \t]*\)|', function( $m ) {
+			return '( ' . preg_replace( '|\s*,\s*|', ', ', $m[1] ) . ' )';
+		}, $code );
+		$code = str_replace( '))', ') )', $code );
+
+		// Fix indenting
+		$code = preg_replace( '|\}\s*else\s*\{|', '} else {', $code );
+		$code = preg_replace( '|^(\s*\S+\S*\{)\s*(?!=\/)(\S+.+?$)|', "$1\n$2", $code );
+		$code = preg_replace_callback( "|^(.+?)$|m", function( $m ) {
+			$i = preg_match( '#(\{|\()($|[ \t]*//)#', $m[1] );
+			$o = preg_match( '#^\s*(\}|\))#', $m[1] );
+			if( $i && $o ) self::$indent--;
+			if( $o && !$i ) self::$indent--;
+			$line = preg_replace( "|^\s*|", self::$indent > 0 ? str_repeat( "\t", self::$indent ) : '', $m[1] );
+			if( $i ) self::$indent++;
+			return $line;
+		}, $code );
+
+		// Single space around operators
+		foreach( self::$nops as self::$i => $nop ) {
+			$code = preg_replace_callback( '#' . preg_quote( $nop ) . '#', function( $m ) {
+				return 'n' . self::$i . self::$uniq;
+			}, $code );
+		}
+		foreach( self::$ops as self::$i => $op ) {
+			$code = preg_replace_callback( '#[ \t]*(' . preg_quote( $op ) . '+)[ \t]*#', function( $m ) {
+				return strlen( $m[1] ) == strlen( self::$ops[self::$i] ) ? 'o' . self::$i . self::$uniq : $m[0];
+			}, $code );
+		}
+		foreach( self::$ops as $nop ) {
+			$code = preg_replace_callback( '#n([0-9]+)' . self::$uniq . '#', function( $m ) {
+				return self::$nops[$m[1]];
+			}, $code );
+		}
+		foreach( self::$ops as $op ) {
+			$code = preg_replace_callback( '#o([0-9]+)' . self::$uniq . '#', function( $m ) {
+				return ' ' . self::$ops[$m[1]] . ' ';
+			}, $code );
+		}
+
+		// Put all the preserved content back in place
+		self::postprocess( $code );
+
+		// Allow only single empty lines
+		$code = preg_replace( '%\n\n+%', "\n\n", $code );
+
+		return trim( $code );
+	}
+
+	/**
+	 * Format the code into a uniform state ready for processing
+	 * - this is done to the whole file not just the PHP,
+	 * - becuase strings and comments may contain symbols that confuse the process
+	 */
+	private static function preprocess( &$code ) {
 
 		// Remove trailing whitespace
 		$code = preg_replace( '|^(.*?)[ \t]+$|m', '$1', $code );
@@ -44,11 +134,11 @@ class CodeTidy {
 		// Protect strings
 		$code = preg_replace_callback( "|'(.+?)'|", function( $m ) {
 			self::$strings1[] = $m[1];
-			return "'s1" . self::$uniq . "'";
+			return "'s1" . ( count( self::$strings1 ) - 1 ) . self::$uniq . "'";
 		}, $code );
 		$code = preg_replace_callback( '|"(.+?)"|', function( $m ) {
 			self::$strings2[] = $m[1];
-			return "\"s2" . self::$uniq . "\"";
+			return "\"s2" . ( count( self::$strings2 ) - 1 ) . self::$uniq . "\"";
 		}, $code );
 
 		// Change old perl-style comments to double-slash
@@ -57,67 +147,40 @@ class CodeTidy {
 		// Protect comments
 		$code = preg_replace_callback( '|(?<=/\*)(.+?)(?=\*/)|s', function( $m ) {
 			self::$comments1[] = $m[1];
-			return 'c1' . self::$uniq;
+			return 'c1' . ( count( self::$comments1 ) - 1 ) . self::$uniq;
 		}, $code );
 		$code = preg_replace_callback( '|(?<=//)(.+?)$|m', function( $m ) {
 			self::$comments2[] = $m[1];
-			return 'c2' . self::$uniq;
+			return 'c2' . ( count( self::$comments2 ) - 1 ) . self::$uniq;
 		}, $code );
+	}
 
-		// Move orphan opening braces to previous line
-		$code = preg_replace( '#(?<=\S)\s*\{[ \t]*$#m', ' {', $code );
-		$code = preg_replace( '#(?<=\S)\s*\{([ \t]*//)#m', ' {$1', $code );
-
-		// Clean up brackets
-		$code = preg_replace_callback( '|\([ \t]*(.+?)[ \t]*\)|', function( $m ) {
-			return '( ' . preg_replace( '|\s*,\s*|', ', ', $m[1] ) . ' )';
-		}, $code );
-
-		// Fix indenting
-		$code = preg_replace_callback( "|^(.+?)$|m", function( $m ) {
-			$i = preg_match( '#(\{|\()($|[ \t]*//)#', $m[1] );
-			$o = preg_match( '#^\s*(\}|\))#', $m[1] );
-			if( $i && $o ) self::$indent--;
-			if( $o && !$i ) self::$indent--;
-			$line = preg_replace( "|^\s*|", self::$indent > 0 ? str_repeat( "\t", self::$indent ) : '', $m[1] );
-			if( $i ) self::$indent++;
-			return $line;
-		}, $code );
-
-		// Single space around operators
-		foreach( self::$ops as self::$i => $op ) {
-			$code = preg_replace_callback( '#[ \t]*(' . preg_quote( $op ) . '+)[ \t]*#', function( $m ) {
-				return strlen( $m[1] ) == strlen( self::$ops[self::$i] ) ? 'o' . self::$i . self::$uniq : $m[0];
-			}, $code );
-		}
-		foreach( self::$ops as $op ) {
-			$code = preg_replace_callback( '#o([0-9]+)' . self::$uniq . '#', function( $m ) {
-				return ' ' . self::$ops[$m[1]] . ' ';
-			}, $code );
-		}
-
-		// Put strings back
-		$code = preg_replace_callback( '|s1' . self::$uniq . '|', function( $m ) {
-			return array_shift( self::$strings1 );
-		}, $code );
-		$code = preg_replace_callback( '|s2' . self::$uniq . '|', function( $m ) {
-			return array_shift( self::$strings2 );
-		}, $code );
+	/**
+	 * Put all the preserved content back in place
+	 */
+	private static function postprocess( &$code ) {
 
 		// Put comments back
-		$code = preg_replace_callback( '|c1' . self::$uniq . '|', function( $m ) {
-			return array_shift( self::$comments1 );
+		$code = preg_replace_callback( '|c1([0-9]+)' . self::$uniq . '|', function( $m ) {
+			return self::$comments1[$m[1]];
 		}, $code );
-		$code = preg_replace_callback( '|c2' . self::$uniq . '|', function( $m ) {
-			return array_shift( self::$comments2 );
+		$code = preg_replace_callback( '|c2([0-9]+)' . self::$uniq . '|', function( $m ) {
+			return self::$comments2[$m[1]];
+		}, $code );
+
+		// Put strings back
+		$code = preg_replace_callback( '|s1([0-9]+)' . self::$uniq . '|', function( $m ) {
+			return self::$strings1[$m[1]];
+		}, $code );
+		$code = preg_replace_callback( '|s2([0-9]+)' . self::$uniq . '|', function( $m ) {
+			return self::$strings2[$m[1]];
 		}, $code );
 
 		// Put escaped quotes back
 		$code = preg_replace( '|q1' . self::$uniq . '|', "\\'", $code );
 		$code = preg_replace( '|q2' . self::$uniq . '|', '\\"', $code );
-
-		return $code;
 	}
+
 }
 
 if( isset( $argv[1] ) ) {
