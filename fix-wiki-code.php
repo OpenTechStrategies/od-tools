038b162969859d82fcafeeebba2d07a58951fd2d
Aran <aran@organicdesign.co.nz>
2015-05-22 13:51:42 -0300

    script to reformat source-code blocks in the wiki

---
+++ fix-wiki-code.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Change the {{code|<LANG> syntax to <source lang="LANG" syntax
+ */
+$path = $_SERVER['argv'][0];
+if( $path[0] != '/' ) die( "Please use absolute path to execute the script so I can assess where the code-base resides.\n" );
+$IP = preg_replace( '|(^.+)/extensions/.+$|', '$1', $path );
+putenv( "MW_INSTALL_PATH=$IP" );
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class FixCode extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Change the {{code|<LANG> syntax to <source lang="LANG" syntax';
+	}
+
+	public function execute() {
+		$dbr = wfGetDB( DB_MASTER );
+		$res  = $dbr->select( 'templatelinks', 'tl_from', array( 'tl_namespace' => NS_TEMPLATE, 'tl_title' => 'Code' ) );
+		foreach( $res as $row ) {
+			$title = Title::newFromId( $row->tl_from );
+			$article = new Article ( $title );
+			$text = $article->getContent();
+			$text = preg_replace_callback( '%\{\{code\|\s*<(.+?)>\s*(.+?)\s*</\\1>\s*\}\}%s', function( $m ) {
+				$lang = $m[1] === 'pre' ? '' : ' lang="' . $m[1] . '"';
+				return "<source$lang>\n$m[2]\n</source>";
+			}, $text );
+			$article->doEdit( $text, 'Change source-code blocks to standard format', EDIT_UPDATE );
+			$this->output( 'Fixed "' . $title->getPrefixedText() . '"\n' );
+		}
+	}
+}
+
+$maintClass = "FixCode";
+require_once RUN_MAINTENANCE_IF_MAIN;
