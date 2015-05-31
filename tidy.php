d4441b8cb60db023d7ee580753fe3d1ea5475373
Aran <aran@organicdesign.co.nz>
2015-05-31 08:06:25 -0300

    remove diffiult rule

--- tidy.php
+++ tidy.php
@@ -305,6 +305,9 @@ class CodeTidy {
 
 		// Special case for prior inc/dec
 		$code = preg_replace( '%(\+\+|--) +\$%', '$1$', $code );
+
+		// Special case for type casts
+		$code = preg_replace( '%\( *([a-z]+) *\) *%', '($1)', $code );
 	}
 
 	/**
@@ -389,11 +392,11 @@ class CodeTidy {
 				// Special case: Case/Default or }...{ subtract 1 from the intented depth
 				if( preg_match( '%^\s*(case |default[ :]|[\)\}].+?[\(\{])%', $line ) ) $n--;
 
-				// Special case: For content ending in a closing bracket add 1 to indented depth
-				if( $bracketLevel < $lastBracketLevel && preg_match( '%[^\s\(\);\{\}]+%', $line ) ) $n++;
+				// Special case: For content ending in a closing bracket add 1 to indented depth (too dodgy)
+				//if( $bracketLevel < $lastBracketLevel && preg_match( '%[^\s\(\);\{\}]+%', $line ) ) $n++;
 
 				// Special case for multi-line statement lines starting with operators
-				if( $state == 0 && preg_match( '%^[ \t]*[-&|+*!,:?]%', $line ) && !preg_match( '%^[ \t]*(\-\-|\+\+)%', $line ) ) $n++;
+				if( $state == 0 && preg_match( '%^[ \t]*[-&|.+*!,:?]%', $line ) && !preg_match( '%^[ \t]*(\-\-|\+\+)%', $line ) ) $n++;
 
 				// Remove any existing indenting (can be some after separating a line with mutlitple statements)
 				$line = preg_replace( '%^\t*%', '', $line );
