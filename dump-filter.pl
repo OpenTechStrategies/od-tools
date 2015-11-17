f70c92906a9d9ef164dafa6248e84d0a1cae6527
Aranad <aran@organicdesign.co.nz>
2015-11-17 19:54:08 -0200

    add args and usage

--- dump-filter.pl
+++ dump-filter.pl
@@ -5,10 +5,22 @@
 # Author: http://www.organicdesign.co.nz/nad
 #
 
-$db = $ARGV[...];
-$tb = $ARGV[...];
-$input = $ARGV[...];
-$output = $ARGV[...];
+$args     = join ' ', @ARGV;
+( $db, $tbl ) = ( $1, $2 ) if $args =~ /--filter=(\w+)\.(\S+)/;
+$rename = $1 if $args =~ /--rename=(\w+)/;
+$input  = $1 if $args =~ /--input=(\S+)/;
+$output = $1 if $args =~ /--output=(\S+)/;
+
+die "\nUsage:\n\n
+	--input=input-dump-file.sql
+
+	--output=output-dump-file.sql
+
+	--filter=dbname.table-prefix | dbname.*
+
+	--rename=new-table-prefix (optional)
+
+" unless defined $input and defined $output and defined $db;
 
 if( open INPUT, '<', $input ) {
 
@@ -20,24 +32,31 @@ if( open INPUT, '<', $input ) {
 	$ourdb = 0;
 	@databases = ();
 
+	# Loop through the input dump pne line at a time
 	for( <INPUT> ) {
 		$line = $_;
 
-		if( $line =~ /CREATE DATABASE `(.+?)`/ ) {
+		# Skip use statements
+		next if $line =~ /^USE /;
+
+		# Create database commands determine whether we're in our part of the dump
+		if( $line =~ /^CREATE DATABASE.+?`(\w+)`/ ) {
 			$head = 0;
 			$ourdb = ( $1 eq $db )
 		}
 
 		else {
 
-			if( $line =~ /CREATE TABLE `(.+?)`/ ) {
+			# Create or drop table commands determine whether we're in our part of the database
+			if( $line =~ /^(DROP TABLE IF EXISTS|CREATE TABLE).+?`(\w+)`/ ) {
 				$head = 0;
-				$ourtbl = ( $1 eq $tbl );
+				$ourtbl = ( $1 eq $tbl or $tbl eq '*' );
 			}
 
+			# If it's the dump header, or we're within our table and database add this line to the output dump
 			if( $head or ( $ourtbl and ( $#databases >=0 or $ourdb ) ) ) {
 
-				$line =~ s/`old/`new/g;
+				$line =~ s/`$tbl/`$rename/g if defined $rename;
 				$found = 1;
 
 				print OUTPUT $line;
