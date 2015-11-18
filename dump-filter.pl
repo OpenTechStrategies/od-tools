af5f61f86c8a0c6880c7667eed49919fec4eff30
Aranad <aran@organicdesign.co.nz>
2015-11-18 12:40:07 -0200

    start debugging and better errors

--- dump-filter.pl
+++ dump-filter.pl
@@ -6,17 +6,17 @@
 #
 
 $args     = join ' ', @ARGV;
-( $db, $tbl ) = ( $1, $2 ) if $args =~ /--filter=(\w+)\.(\S+)/;
+( $db, $tbl ) = ( $1, $2 ) if $args =~ /--filter=(\S+)\.(\S+)/;
 $rename = $1 if $args =~ /--rename=(\w+)/;
 $input  = $1 if $args =~ /--input=(\S+)/;
 $output = $1 if $args =~ /--output=(\S+)/;
 
-die "\nUsage:\n\n
+die "\nUsage:
 	--input=input-dump-file.sql
 
 	--output=output-dump-file.sql
 
-	--filter=dbname.table-prefix | dbname.*
+	--filter=dbname.table-prefix | dbname.* | *.table-prefix
 
 	--rename=new-table-prefix (optional)
 
@@ -24,7 +24,7 @@ die "\nUsage:\n\n
 
 if( open INPUT, '<', $input ) {
 
-	open OUTPUT, '>', $output
+	open OUTPUT, '>', $output;
 	
 	$found = 0;
 	$head = 1;
@@ -33,7 +33,7 @@ if( open INPUT, '<', $input ) {
 	@databases = ();
 
 	# Loop through the input dump pne line at a time
-	for( <INPUT> ) {
+	while( <INPUT> ) {
 		$line = $_;
 
 		# Skip use statements
@@ -41,34 +41,35 @@ if( open INPUT, '<', $input ) {
 
 		# Create database commands determine whether we're in our part of the dump
 		if( $line =~ /^CREATE DATABASE.+?`(\w+)`/ ) {
+			push @databases, $1;
 			$head = 0;
 			$ourdb = ( $1 eq $db )
-		}
-
-		else {
+		} else {
 
 			# Create or drop table commands determine whether we're in our part of the database
-			if( $line =~ /^(DROP TABLE IF EXISTS|CREATE TABLE).+?`(\w+)`/ ) {
+			if( $line =~ /^(-- Table structure for table|DROP TABLE IF EXISTS|CREATE TABLE).+?`(\w+)`/ ) {
 				$head = 0;
-				$ourtbl = ( $1 eq $tbl or $tbl eq '*' );
+				$ourtbl = ( $1 =~ /^$tbl/ or $tbl eq '*' );
 			}
 
 			# If it's the dump header, or we're within our table and database add this line to the output dump
-			if( $head or ( $ourtbl and ( $#databases >=0 or $ourdb ) ) ) {
-
+			if( $head or ( $ourtbl and ( $#databases < 0 or $ourdb ) ) ) {
 				$line =~ s/`$tbl/`$rename/g if defined $rename;
-				$found = 1;
-
+				$found = 1 unless $head;
 				print OUTPUT $line;
-
 			}
-
-
 		}
-
+	}
 	close INPUT;
 	close OUTPUT;
 }
 
-print "\"$tbl\" not found\n" unless $found;
-print "This is a multi-database dump containing the following databases: " . join( ', ', @databases ) . "\n" if $#databases >= 0;
+if( $found ) { print "\nSuccess.\n" }
+else {
+	print "\nNo tables with prefix \"$tbl\" found.\n" unless $found;
+	if( $#databases >= 0 ) {
+		print "\nThis is a multi-database dump containing the following databases:\n\t" . join( "\n\t", @databases ) . "\n";
+		print "\nYou can't use '*' for the database name with a multi-database dump.\n" if $db eq '*';
+	}
+	elsif( $db ne '*' ) { print "\nThis dump contains only a single database, so you must use '*' for the database name.\n" }
+}
