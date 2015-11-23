80354ce824024923d218a106f13fbaafcdb29d14
Aranad <aran@organicdesign.co.nz>
2015-11-23 13:12:09 -0200

    allow filtering users who use this

--- copy-to-sent.pl
+++ copy-to-sent.pl
@@ -11,49 +11,53 @@ if( open FH,'<', $file ) {
 	if( $users =~ /^$sender\s*:\s*(.+?)\@localhost\s*$/m ) {
 		$user = $1;
 
-		# Scan the new messages in their Sent folder
-		for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
-			print FH "$msg\n";
-			if( open FMSG,'<', $msg ) {
-				
-				# Read the message header
-				sysread FMSG, $content, 600;
-				close FMSG;
+		# Filter the users who use web-mail and have it copy to sent for them
+		if( $user ne 'beth' ) {
 
-				# Check if its ours by ID
-				if( $content =~ /\s$id\s/s ) {
-
-					# Get the whole message
-					open FMSG,'<', $msg;
-					sysread FMSG, $content, -s $msg;
+			# Scan the new messages in their Sent folder
+			for my $msg (glob "/home/$user/Maildir/.Sent/new/*") {
+				print FH "$msg\n";
+				if( open FMSG,'<', $msg ) {
+					
+					# Read the message header
+					sysread FMSG, $content, 600;
 					close FMSG;
 
-					# Turn the To and CC headers into lists and then hashes
-					$to = $content =~ /^\s*To:\s*(.+?)\s+(\w: )/mis ? $1 : '';
-					@to = $to =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
-					%to = map { $_ => 1 } @to;
-					$cc = $content =~ /^\s*CC:\s*(.+?)\s+(\w: )/mis ? $1 : '';
-					@cc = $cc =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
-					%cc = map { $_ => 1 } @cc;
+					# Check if its ours by ID
+					if( $content =~ /\s$id\s/s ) {
 
-					# Build a Bcc header from all the recipients not in the To or Cc headers
-					@bcc = ();
-					for(@recipients) {
-						push @bcc, $_ unless exists $to{$_} or exists $cc{$_};
-					}
-					$bcc = $#bcc < 0 ? 0 : 'Bcc: ' . join(', ', @bcc);
-					
-					# Add the Bcc header after the To header
-					$content =~ s/(^\s*To:.+?$)/$1\n$bcc/mi if $bcc;
-
-					# Write the new content to the file
-					if(open FMSG,'>', $msg) {
-						syswrite FMSG, $content;
+						# Get the whole message
+						open FMSG,'<', $msg;
+						sysread FMSG, $content, -s $msg;
 						close FMSG;
-					}
 
-					# Mark as read
-					rename $msg, "$msg:2,S";
+						# Turn the To and CC headers into lists and then hashes
+						$to = $content =~ /^\s*To:\s*(.+?)\s+(\w: )/mis ? $1 : '';
+						@to = $to =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
+						%to = map { $_ => 1 } @to;
+						$cc = $content =~ /^\s*CC:\s*(.+?)\s+(\w: )/mis ? $1 : '';
+						@cc = $cc =~ /([0-9a-z_.&-]+@[0-9a-z_.&-]+)/gi;
+						%cc = map { $_ => 1 } @cc;
+
+						# Build a Bcc header from all the recipients not in the To or Cc headers
+						@bcc = ();
+						for(@recipients) {
+							push @bcc, $_ unless exists $to{$_} or exists $cc{$_};
+						}
+						$bcc = $#bcc < 0 ? 0 : 'Bcc: ' . join(', ', @bcc);
+						
+						# Add the Bcc header after the To header
+						$content =~ s/(^\s*To:.+?$)/$1\n$bcc/mi if $bcc;
+
+						# Write the new content to the file
+						if(open FMSG,'>', $msg) {
+							syswrite FMSG, $content;
+							close FMSG;
+						}
+
+						# Mark as read
+						rename $msg, "$msg:2,S";
+					}
 				}
 			}
 		}
