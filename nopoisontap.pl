c95bc2277f7d5c545b7ecdfb3fe31b1a02419267
Aranad <aran@organicdesign.co.nz>
2016-11-19 08:56:44 -0200

    use system

--- nopoisontap.pl
+++ nopoisontap.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Used to disable networking when screen is locked (see organicdesign.co.nz/PoisonTap_solution
 if( $ARGV[0] eq 'start' ) {
-	qx( dbus-monitor --session "type=signal,interface=org.gnome.SessionManager.Presence,member=StatusChanged" | $0 & );
+	system( "dbus-monitor --session \"type=signal,interface=org.gnome.SessionManager.Presence,member=StatusChanged\" | $0 &" );
 } else {
 	while( <> ) {
 		`nmcli nm enable false` if /uint32 3/;
