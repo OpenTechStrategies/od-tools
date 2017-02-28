9637dab9f67d2af34c453eb9e6f04d869494f173
Karl Fogel <kfogel@red-bean.com>
2017-02-28 17:09:50 -0600

    Finish support for our MediaWiki import use case
    Complete support for multi-line fields, begun in commit b8fbd0aade.
    Remove a lot of code that our use case doesn't need, including fancy
    titles and various unnecessary run-time job control parameters.  Print
    out a MediaWiki-syntax TOC listing all the pages created.  Remove
    debugging statements.  Adjust copyright and provenance notices.

--- csv2wiki.pl
+++ csv2wiki.pl
@@ -2,6 +2,9 @@
 #
 # Copyright (C) 2008-2010 Aran Dunkley, Marcus Davy and others.
 #
+# Copyright (C) 2017 Open Tech Strategies, LLC  (modifications:
+#   https://github.com/OpenTechStrategies/od-tools/blob/master/csv2wiki.pl)
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -17,12 +20,9 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 # http://www.gnu.org/copyleft/gpl.html
 #
-# - Source:  http://www.organicdesign.co.nz/csv2wiki.pl
+# - Original Source:  http://www.organicdesign.co.nz/csv2wiki.pl
 # - Started: 2008-03-21
  
-# Todo
-# Make it so that if there is no title then it increments
-# $hashref = { $wikitext =~ /\{{3}(.+?)(\|.*?)?\}{3}/g }
 require( './wiki.pl' );
 
 use Text::CSV;
@@ -36,13 +36,7 @@ $ARGV[0] =~ /^(.+?)(\..+?)?$/;
 $log = "$1.log";
 $err = "$1.err";
 $sep = ",";
-$multisep = "\n";
-$title = 0;
-$template = "Record";
-$replace = 0;
-$append = 0;
-$titleformat = "";
-%dups = ();
+$title_prefix = "Record-";
 
 # Parse the job file
 if ( open JOB, '<', $ARGV[0] ) {
@@ -51,12 +45,8 @@ if ( open JOB, '<', $ARGV[0] ) {
 		if ( /^\*?\s*\$?wiki\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )         { $wiki = $1 }
 		if ( /^\*?\s*\$?user\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )         { $user = $1 }
 		if ( /^\*?\s*\$?pass\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )         { $pass = $1 }
+		if ( /^\*?\s*\$?title_prefix\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i ) { $title_prefix = $1 }
 		if ( /^\*?\s*\$?sep(arator)?\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i ) { $sep = $2 }
-		if ( /^\*?\s*\$?multisep\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )     { $multisep = $1 }
-		if ( /^\*?\s*\$?title\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )        { $titleformat = $1 }
-		if ( /^\*?\s*\$?template\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )     { $template = $1 } 
-		if ( /^\*?\s*\$?replace\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )      { $replace = $1 }
-		if ( /^\*?\s*\$?append\s*[:=]\s*['"]?(.+?)['"]?;?\s*$/i )       { $append = $1 }
 	}
 	close JOB;
 } else { die "Couldn't parse job file!" }
@@ -71,80 +61,44 @@ my $csv_parser = Text::CSV->new({ binary => 1 });
 # Process the records
 my %lut = ();
 my $wptr = 0;
+my @headings;
 
 open(my $csv_stream, '<', $csv) or die "Couldn't open input file '$csv'!";
 while ( my $row = $csv_parser->getline ($csv_stream) ) {
 	my @fields = @$row;
+        my $text = "";
 
-        print "KFF:\n";
-        print "$fields[0]: $fields[5] ($fields[4])\n";
-        print "$#fields\n";
-        print "\n";
-
-	# If this is the first row, define the columns and a reverse lookup
+	# If this is the first row, define the page headings.
 	if ( $wptr == 0 ) {
-		# TODO: Some code removed here might actually have been
-		# important for creating the page later.  Check back.
 		$lut{lc $fields[$_]} = $_ for 0 .. $#fields;
+                @headings = @fields;
 	}
-	else {  # Build the record as wikitext template syntax
-		my $tmpl = "{{$template";
+	else {  # Build the record
 		my $i = -1;
 		for ( @fields ) {
-			s/^\s*(.+?)\s*$/$1/g;
-			$tmpl .= $fields[++$i] ? "\n | $fields[$i] = $_" : ( $_ ? $multisep . $_ : '' );
-		}
-		$tmpl .= "\n}}";
-
-		# Determine title for the record
-		my $title = $titleformat;
-		if ( $title ) {
-			$title =~ s/\$(\w+)/ buildTitle( $1, \@fields ) /eg;
-		} else {
-			$title = wikiGuid();
+			# s/^\s*(.+?)\s*$/$1/g;
+			$text .= "== " . $headings[++$i] . " ==\n\n";
+			$text .= "$_";
+			$text .= "\n";
 		}
 
-		if ( $replace ) {
-			$text = $tmpl;
-		} else {
-
-			# Find the template in the wikitext if exists
-			$text = wikiRawPage( $wiki, $title );
-			for ( wikiExamineBraces( $text ) ) {
-				( $pos, $len ) = ( $_->{OFFSET}, $_->{LENGTH} ) if $_->{NAME} eq $template;
-			}
-
-			# Replace, prepend or append the template into the current text 
-			if ( defined $pos ) {
-				substr $text, $pos, $len, $tmpl;
-			} else {
-				$text = $append ? "$text\n$tmpl" : "$tmpl\n$text";
-			}
-		}
-
-		# Append (n) if duplicate title
-		$n = ++$dups{$title};
-		$title = "$title ($n)" if $n > 1;
+                my $printable_number = sprintf("%03d", $wptr);
+		$title = "${title_prefix}${printable_number}";
 
 		# Update the article
-		print "Processing row $wptr ($title)\n";
-		my $comment = "[[Template:$::template|$::template]] replacement using csv2wiki.pl";
+		my $comment = "Generate this page using csv2wiki.pl.";
 		wikiEdit( $wiki, $title, $text, $comment );
+
+		# TODO: This part is specific to OTS's particular use
+		# case.  It causes a MediaWiki-formatted list of all
+		# the pages created to be printed on stdout.  The idea
+		# is that you would take that output and paste it
+		# directly into a top-level wiki page (e.g., "Proposals")
+		# to serve as the TOC for all the created pages.  It
+		# would be nice to generalize this so that it were
+		# somehow controlled by the job file in a customizable way.
+		print("* [[$title]]: '''$fields[0]''', $fields[5] ([$fields[4] YouTube])\n");
 	}
 
 	$wptr++;
 }
- 
-close CSV;
-
-# Replace a token from the title format string with data
-# - named indexes are case-insensitive
-# - removes double spaces from result
-sub buildTitle {
-	my $i = shift;
-	my $data = shift;
-	my $title = $$data[ $i =~ /\D/ ? $::lut{lc $i} : $i - 1 ];
-	$title =~ s/ +/ /g;
-	return $title;
-}
-
